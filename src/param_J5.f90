d501da98de207645bd7c0b4a7ee7b7ade51c3383
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-03-28 12:28:22 -0400

    Modify DOMAIN_LEVEL so PATCH_LEVEL>=2

--- src/param_J5.f90
+++ src/param_J5.f90
@@ -1,5 +1,6 @@
 module param_mod
   ! Jmin = DOMAIN_LEVEL + PATCH_LEVEL + 1
-  integer, parameter :: DOMAIN_LEVEL = 3
+  ! Max number of cores = 10 4^DOMAIN_LEVEL, PATCH_LEVEL>=2
+  integer, parameter :: DOMAIN_LEVEL = 2
   integer, parameter :: PATCH_LEVEL = 4 - DOMAIN_LEVEL
 end module param_mod
