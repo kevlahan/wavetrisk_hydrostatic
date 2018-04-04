ecda9ccc7c7ccf91d70d9f24a52aa8e53dc3904d
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-04-04 16:50:02 -0400

    Modify remap for adaptive case.

--- src/remap.f90
+++ src/remap.f90
@@ -7,6 +7,7 @@ module remap_mod
   
   integer, parameter          :: order = 3
   integer, dimension(1:order) :: stencil
+  real(8), parameter :: ex_val = -1.0d13
 contains
   subroutine remap_vertical_coordinates (set_thresholds)
     ! Remap the Lagrangian layers to initial vertical grid given a_vert and b_vert vertical coordinate parameters 
@@ -21,11 +22,16 @@ contains
 
     ! Ensure boundary values are up to date
     call update_array_bdry (sol, NONE)
+    
+    do k = 1, zlevels
+       do d = 1, size(grid)
+          exner_fun(k)%data(d)%elts = ex_val
+       end do
+    end do
 
     ! Remap on finest level
-    call apply_onescale (remap_eta,   level_end, z_null, 0, 1)
-    call apply_onescale (remap_theta, level_end, z_null, 0, 1)
-    call apply_onescale (remap_velo,  level_end, z_null, 0, 0)
+    call apply_onescale (remap_scalars, level_end, z_null, 0, 1)
+    call apply_onescale (remap_velo,    level_end, z_null, 0, 0)
     
     ! Remap scalars at coarser levels
     do l = level_end-1, level_start-1, -1
@@ -45,9 +51,8 @@ contains
        call update_array_bdry (wav_coeff(S_MASS:S_TEMP,:), l+1)
 
        ! Remap at level l (over-written if value available from restriction)
-       call apply_onescale (remap_eta,   l, z_null, 0, 1)
-       call apply_onescale (remap_theta, l, z_null, 0, 1)
-       call apply_onescale (remap_velo,  l, z_null, 0, 0)
+       call apply_onescale (remap_scalars, l, z_null, 0, 1)
+       call apply_onescale (remap_velo,    l, z_null, 0, 0)
 
        ! Restrict scalars (sub-sample and lift) and velocity (average) to coarser grid
        do d = 1, size(grid)
@@ -93,7 +98,7 @@ contains
     call update_array_bdry (sol_save, NONE)
   end subroutine remap_save
   
-  subroutine remap_eta (dom, i, j, z_null, offs, dims)
+  subroutine remap_scalars (dom, i, j, z_null, offs, dims)
     ! Remap mass and vertical variable eta onto grid defined by A, B coefficients
     type (Domain)                   :: dom
     integer                         :: i, j, z_null
@@ -101,8 +106,9 @@ contains
     integer, dimension (2,N_BDRY+1) :: dims
 
     integer                          :: current_zlev, d, id, id_i, k, zlev
-    real(8)                          :: column_mass, cumul_mass_zlev, cumul_mass_target, new_cumul_mass, cumul_mass_upper
-    real(8), dimension (zlevels+1)   :: eta, cumul_mass
+    real(8)                          :: column_mass, cumul_mass_zlev, cumul_mass_target, new_cumul_mass, cumul_mass_upper, X
+    real(8)                          :: new_mass
+    real(8), dimension (zlevels+1)   :: eta, cumul_mass, cumul_temp, new_cumul_temp
 
     d    = dom%id + 1
     id   = idx(i, j, offs, dims)
@@ -110,20 +116,23 @@ contains
 
     ! Calculate cumulative mass and total mass of column
     cumul_mass(1) = 0.0_8
+    cumul_temp(1) = 0.0_8
     do k = 1, zlevels
        cumul_mass(k+1) = cumul_mass(k) + sol(S_MASS,k)%data(d)%elts(id_i)
+       cumul_temp(k+1) = cumul_temp(k) + sol(S_TEMP,k)%data(d)%elts(id_i)
     end do
     column_mass = cumul_mass(zlevels+1)
-
+    
     current_zlev = 1
     exner_fun(1)%data(d)%elts(id_i) = 1.0_8
     new_cumul_mass = 0.0_8
     do k = 1, zlevels
        ! Save old mass in trend
        trend(S_MASS,k)%data(d)%elts(id_i) = sol(S_MASS,k)%data(d)%elts(id_i)
+
+       new_mass = a_vert_mass(k) + b_vert_mass(k) * column_mass
+       if (new_mass<0.0_8) return ! Do not try to remap pole excpet at coarsest level
        
-       ! New mass
-       sol(S_MASS,k)%data(d)%elts(id_i) = a_vert_mass(k) + b_vert_mass(k) * column_mass
        cumul_mass_target = new_cumul_mass + sol(S_MASS,k)%data(d)%elts(id_i)
 
        do zlev = current_zlev, zlevels
@@ -140,30 +149,10 @@ contains
        ! New vertical coordinate (saved in exner_fun)
        exner_fun(k+1)%data(d)%elts(id_i) = zlev + (cumul_mass_target - cumul_mass_zlev)/(cumul_mass_upper - cumul_mass_zlev)
     end do
-  end subroutine remap_eta
 
-  subroutine remap_theta (dom, i, j, z_null, offs, dims)
-    ! Remap mass weighted potential temperature to original vertical grid using linear interpolation on cumulative values
-    type (Domain)                   :: dom
-    integer                         :: i, j, z_null
-    integer, dimension (N_BDRY+1)   :: offs
-    integer, dimension (2,N_BDRY+1) :: dims
-
-    integer :: d, id, id_i, k, zlev
-    real(8) :: X
-    real(8), dimension(zlevels+1) :: cumul_temp, new_cumul_temp
-
-    d    = dom%id + 1
-    id   = idx(i, j, offs, dims)
-    id_i = id + 1
-
-    cumul_temp(1) = 0.0_8
-    do k = 1, zlevels
-       cumul_temp(k+1) = cumul_temp(k) + sol(S_TEMP,k)%data(d)%elts(id_i)
-    end do
-
+    ! Remap theta
     do k = 1, zlevels+1
-       X = exner_fun(k)%data(d)%elts(id_i) 
+       X = exner_fun(k)%data(d)%elts(id_i)
        zlev = min(zlevels,floor(X))
        X = X - zlev
        new_cumul_temp(k) = cumul_temp(zlev) + X*sol(S_TEMP,zlev)%data(d)%elts(id_i)
@@ -172,7 +161,7 @@ contains
     do k = 1, zlevels
        sol(S_TEMP,k)%data(d)%elts(id_i) = new_cumul_temp(k+1) - new_cumul_temp(k)
     end do
-  end subroutine remap_theta
+  end subroutine remap_scalars
 
   subroutine remap_velo (dom, i, j, z_null, offs, dims)
     ! Remap velocity onto original vertical grid by linear interpolation of mass flux
@@ -199,6 +188,11 @@ contains
        mass_e(RT+1) = trend(S_MASS,k)%data(d)%elts(id_i) + trend(S_MASS,k)%data(d)%elts(idE)
        mass_e(DG+1) = trend(S_MASS,k)%data(d)%elts(id_i) + trend(S_MASS,k)%data(d)%elts(idNE)
        mass_e(UP+1) = trend(S_MASS,k)%data(d)%elts(id_i) + trend(S_MASS,k)%data(d)%elts(idN)
+
+       if (exner_fun(k)%data(d)%elts(idE).eq.ex_val)  mass_e(RT+1) = trend(S_MASS,k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idNE).eq.ex_val) mass_e(DG+1) = trend(S_MASS,k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idN).eq.ex_val)  mass_e(UP+1) = trend(S_MASS,k)%data(d)%elts(id_i)
+       
        do e = 1, EDGE
           massflux(k,e) = sol(S_VELO,k)%data(d)%elts(EDGE*id+e) * mass_e(e)
           massflux_cumul(k+1,e) = massflux_cumul(k,e) + massflux(k,e)
@@ -209,6 +203,11 @@ contains
        X(RT+1) = 0.5_8*(exner_fun(k)%data(d)%elts(id_i) + exner_fun(k)%data(d)%elts(idE))
        X(DG+1) = 0.5_8*(exner_fun(k)%data(d)%elts(id_i) + exner_fun(k)%data(d)%elts(idNE))
        X(UP+1) = 0.5_8*(exner_fun(k)%data(d)%elts(id_i) + exner_fun(k)%data(d)%elts(idN))
+
+       if (exner_fun(k)%data(d)%elts(idE).eq.ex_val) X(RT+1) = exner_fun(k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idNE).eq.ex_val) X(DG+1) = exner_fun(k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idN).eq.ex_val) X(UP+1) = exner_fun(k)%data(d)%elts(id_i)
+       
        do e = 1, EDGE
           zlev = min(zlevels,floor(X(e)))
           X(e) = X(e) - zlev
@@ -221,6 +220,9 @@ contains
        mass_e(RT+1) = sol(S_MASS,k)%data(d)%elts(id_i) + sol(S_MASS,k)%data(d)%elts(idE)
        mass_e(DG+1) = sol(S_MASS,k)%data(d)%elts(id_i) + sol(S_MASS,k)%data(d)%elts(idNE)
        mass_e(UP+1) = sol(S_MASS,k)%data(d)%elts(id_i) + sol(S_MASS,k)%data(d)%elts(idN)
+       if (exner_fun(k)%data(d)%elts(idE).eq.ex_val)  mass_e(RT+1) = sol(S_MASS,k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idNE).eq.ex_val) mass_e(DG+1) = sol(S_MASS,k)%data(d)%elts(id_i)
+       if (exner_fun(k)%data(d)%elts(idN).eq.ex_val)  mass_e(UP+1) = sol(S_MASS,k)%data(d)%elts(id_i)
        do e = 1, EDGE
           sol(S_VELO,k)%data(d)%elts(EDGE*id+e) = (new_massflux_cumul(k+1,e) - new_massflux_cumul(k,e)) / mass_e(e)
        end do
