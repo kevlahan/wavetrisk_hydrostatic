65c4a1b7ac62b6d315958364034693b84a59b0ef
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-05-18 10:29:55 -0400

    Cleanup.

--- src/base_mpi-lb.f90
+++ src/base_mpi-lb.f90
@@ -3,36 +3,29 @@ module arch_mod
   use geom_mod
   implicit none
   include 'mpif.h'
-  integer rank
-  integer n_process
-  integer ierror
-  integer, dimension(N_GLO_DOMAIN) :: loc_id
-  integer, dimension(N_GLO_DOMAIN) :: owner
-  integer, allocatable :: glo_id(:,:)
-
+  integer                              :: ierror, n_process, rank
+  integer, dimension(N_GLO_DOMAIN)     :: loc_id, owner
+  integer, dimension(:,:), allocatable :: glo_id
 contains
+  subroutine distribute_grid (k)
+    implicit none
+    integer :: k
 
-  subroutine distribute_grid(k)
-    integer k
-
-    integer vwgt(N_GLO_DOMAIN)
-    integer adj_line(N_GLO_DOMAIN)
-    integer i, d, r, d_ngb
-    integer n_domain_floor
-    character(5+4) filename
-    integer :: fid = 599
-    integer total_wgt
-    real(8) wgt_per_rank, wgt_cur_rank, accepted_inbalance
-
+    integer                          :: i, d, r, d_ngb, n_domain_floor, total_wgt
+    integer, dimension(N_GLO_DOMAIN) :: adj_line, vwgt
+    integer, parameter               :: fid = 599
+    character(5+4)                   :: filename
+    real(8)                          :: wgt_per_rank, wgt_cur_rank, accepted_inbalance
+    
     write(filename, '(A,I4.4)')  "conn.", k
 
-    if (k .ge. 0 .and. n_process .gt. 1) then
+    if (k >= 0 .and. n_process > 1) then
 
-       open(unit=fid, file=filename)
+       open (unit=fid, file=filename)
        do d = 1, N_GLO_DOMAIN
-          read(fid,*) vwgt(d), adj_line
+          read (fid,*) vwgt(d), adj_line
        end do
-       close(fid)
+       close (fid)
 
        total_wgt = sum(vwgt)
        wgt_per_rank = dble(total_wgt)/dble(n_process)
@@ -44,31 +37,29 @@ contains
 
        accepted_inbalance = 0.1_8
 
-       do while (d .lt. N_GLO_DOMAIN) ! increase accepted_inbalance until all domains fit
+       do while (d < N_GLO_DOMAIN) ! increase accepted_inbalance until all domains fit
           d = 0
           do r = 1, n_process
              wgt_cur_rank = 0
-             do while (wgt_cur_rank .lt. wgt_per_rank .and. n_process - r .lt. N_GLO_DOMAIN - d)
+             do while (wgt_cur_rank < wgt_per_rank .and. n_process - r < N_GLO_DOMAIN - d)
                 owner(d+1) = r-1
                 wgt_cur_rank = wgt_cur_rank + vwgt(d+1)
                 d = d + 1
              end do
-             ! if load too much, keep last item for next rank
-             if (wgt_cur_rank .gt. dble(wgt_per_rank)*(1.0_8 + accepted_inbalance)) then
-                d = d - 1
-             end if
+             ! If load too much, keep last item for next rank
+             if (wgt_cur_rank > dble(wgt_per_rank)*(1.0_8 + accepted_inbalance)) d = d - 1
           end do
-          ! did not find enough room for all domains > accepted_inbalance was too tight
+          ! Did not find enough room for all domains > accepted_inbalance was too tight
           accepted_inbalance = accepted_inbalance*2.0_8
        end do
-       if (rank .eq. 0) write(*,'(A,es12.4)') 'Accepted imbalance:', accepted_inbalance / 2.0_8
+       if (rank == 0) write(*,'(A,es12.4)') 'Accepted imbalance:', accepted_inbalance/2.0_8
     else
        n_domain_floor = N_GLO_DOMAIN/n_process
        d = 0
        do r = 1, n_process
           owner(d+1:d+n_domain_floor) = r-1
           d = d + n_domain_floor
-          if (r .le. N_GLO_DOMAIN - n_process*n_domain_floor) then
+          if (r <= N_GLO_DOMAIN - n_process*n_domain_floor) then
              owner(d+1) = r-1
              d = d + 1
           end if
@@ -82,42 +73,40 @@ contains
        n_domain(r+1) = n_domain(r+1) + 1
     end do
 
-    if (allocated(glo_id)) deallocate(glo_id)
-    allocate(glo_id(n_process,maxval(n_domain)))
+    if (allocated(glo_id)) deallocate (glo_id)
+    allocate (glo_id(n_process,maxval(n_domain)))
 
     do d = 1, N_GLO_DOMAIN
        glo_id(owner(d)+1,loc_id(d)+1) = d - 1
     end do
-
   end subroutine distribute_grid
 
-  subroutine init_arch_mod()
+  subroutine init_arch_mod
+    implicit none
     logical :: initialized = .False.
 
     if (initialized) return ! initialize only once
 
-    call init_shared_mod()
+    call init_shared_mod
 
-    call MPI_Init(ierror)
-    call MPI_Comm_Size(MPI_COMM_WORLD, n_process, ierror)
-    call MPI_Comm_Rank(MPI_COMM_WORLD, rank, ierror)
+    call MPI_Init (ierror)
+    call MPI_Comm_Size (MPI_COMM_WORLD, n_process, ierror)
+    call MPI_Comm_Rank (MPI_COMM_WORLD, rank, ierror)
 
-    allocate(n_domain(n_process))
+    allocate (n_domain(n_process))
 
     initialized = .True.
-
   end subroutine init_arch_mod
 
-  subroutine finalize()
-    call MPI_Finalize(ierror)
+  subroutine finalize
+    call MPI_Finalize (ierror)
   end subroutine finalize
 
-  subroutine barrier()
-    call MPI_Barrier(MPI_Comm_World, ierror)
+  subroutine barrier
+    call MPI_Barrier (MPI_Comm_World, ierror)
   end subroutine barrier
 
-  subroutine abort()
-    call MPI_Abort(MPI_Comm_World, 1, ierror)
+  subroutine abort
+    call MPI_Abort (MPI_Comm_World, 1, ierror)
   end subroutine abort
-
 end module arch_mod
--- src/base_mpi.f90
+++ src/base_mpi.f90
@@ -3,26 +3,21 @@ module arch_mod
   use geom_mod
   implicit none
   include 'mpif.h'
-  integer rank
-  integer n_process
-  integer ierror
-  integer, dimension(N_GLO_DOMAIN) :: loc_id
-  integer, dimension(N_GLO_DOMAIN) :: owner
-  integer, allocatable :: glo_id(:,:)
-
+  integer                              :: ierror, n_process, rank
+  integer, dimension(N_GLO_DOMAIN)     :: loc_id, owner
+  integer, dimension(:,:), allocatable :: glo_id
 contains
-
-  subroutine distribute_grid(k)
-    integer k
-    integer i, d, r, d_ngb
-    integer n_domain_floor
+  subroutine distribute_grid (k)
+    implicit none
+    integer :: k
+    integer :: i, d, r, d_ngb, n_domain_floor
 
     n_domain_floor = N_GLO_DOMAIN/n_process
     d = 0
     do r = 1, n_process
        owner(d+1:d+n_domain_floor) = r-1
        d = d + n_domain_floor
-       if (r .le. N_GLO_DOMAIN - n_process*n_domain_floor) then
+       if (r <= N_GLO_DOMAIN - n_process*n_domain_floor) then
           owner(d+1) = r-1
           d = d + 1
        end if
@@ -35,42 +30,39 @@ contains
        n_domain(r+1) = n_domain(r+1) + 1
     end do
 
-    if (.not. allocated(glo_id)) then
-       allocate(glo_id(n_process,maxval(n_domain)))
-    end if
+    if (.not. allocated(glo_id)) allocate(glo_id(n_process,maxval(n_domain)))
 
     do d = 1, N_GLO_DOMAIN
        glo_id(owner(d)+1,loc_id(d)+1) = d - 1
     end do
-
   end subroutine distribute_grid
 
-  subroutine init_arch_mod()
+  subroutine init_arch_mod
+    implicit none
     logical :: initialized = .False.
 
     if (initialized) return ! initialize only once
 
-    call init_shared_mod()
+    call init_shared_mod
 
-    call MPI_Init(ierror)
-    call MPI_Comm_Size(MPI_COMM_WORLD, n_process, ierror)
-    call MPI_Comm_Rank(MPI_COMM_WORLD, rank, ierror)
+    call MPI_Init (ierror)
+    call MPI_Comm_Size (MPI_COMM_WORLD, n_process, ierror)
+    call MPI_Comm_Rank (MPI_COMM_WORLD, rank, ierror)
 
-    allocate(n_domain(n_process))
+    allocate (n_domain(n_process))
 
     initialized = .True.
-
   end subroutine init_arch_mod
 
-  subroutine finalize()
-    call MPI_Finalize(ierror)
+  subroutine finalize
+    call MPI_Finalize (ierror)
   end subroutine finalize
 
-  subroutine barrier()
+  subroutine barrier
     call MPI_Barrier(MPI_Comm_World, ierror)
   end subroutine barrier
 
-  subroutine abort()
+  subroutine abort
     call MPI_Abort(MPI_Comm_World, 1, ierror)
   end subroutine abort
 end module arch_mod
