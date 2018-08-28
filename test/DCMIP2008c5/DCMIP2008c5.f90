d55f61f23aa451096ce97c238d3fef692e419f7e
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-08-28 11:41:00 +0200

    Change to eigenvalue viscosity calculation.

--- test/DCMIP2008c5/DCMIP2008c5.f90
+++ test/DCMIP2008c5/DCMIP2008c5.f90
@@ -61,14 +61,14 @@ program DCMIP2008c5
 
   ! Initialize thresholds to default values 
   call initialize_thresholds
-
-  ! Initialize time step and viscosities
-  call initialize_dt_viscosity
     
   ! Initialize variables
   call initialize (apply_initial_conditions, set_thresholds, dump, load, test_case)
   call sum_total_mass (.true.)
   call barrier
+
+  ! Initialize time step and viscosities
+  call initialize_dt_viscosity
 
   ! Save initial conditions
   call write_and_export (iwrite)
