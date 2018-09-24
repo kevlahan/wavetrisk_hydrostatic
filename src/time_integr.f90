f46e4852fa50c65bc2092908ce728415a2a4fd32
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-09-24 10:17:20 +0200

    Cleanup and add separate memory management for RK4 (needs fewer dynamic arrays).

--- src/time_integr.f90
+++ src/time_integr.f90
@@ -15,36 +15,36 @@ contains
     call manage_RK_mem
 
     call trend_fun (sol, trend) 
-    call RK_sub_step1 (sol, trend, 1.0_8, dt/2.0_8, q1)
+    call RK_sub_step1 (sol, trend, 1.0_8, dt/2, q1)
     call WT_after_step (q1, wav_coeff)
 
     call trend_fun (q1, trend) 
-    call RK_sub_step1 (q1, trend, 1.0_8, dt/2.0_8, q2)
+    call RK_sub_step1 (q1, trend, 1.0_8, dt/2, q2)
     call WT_after_step (q2, wav_coeff)
 
     call trend_fun (q2, trend)
-    call RK_sub_step2 (sol, q2, trend, (/ 2.0_8/3.0_8, 1.0_8/3.0_8 /), dt/6.0_8, q3)
+    call RK_sub_step2 (sol, q2, trend, (/ 2.0_8/3, 1.0_8/3 /), dt/6, q3)
     call WT_after_step (q3, wav_coeff)
     
     call trend_fun (q3, trend) 
-    call RK_sub_step1 (q3, trend, 1.0_8, dt/2.0_8, sol)
+    call RK_sub_step1 (q3, trend, 1.0_8, dt/2, sol)
     call WT_after_step (sol, wav_coeff, level_start-1)
   end subroutine RK34_opt
 
   subroutine RK45_opt (trend_fun, dt)
     ! Fourth order, five stage strong stability preserving Runge-Kutta method stable with optimal maximum CFL coefficient of 1.51
-    ! See A. Balan, G. May and J. Schoberl: "A Stable Spectral Difference Method for Triangles", 2011, Spiter and Ruuth 2002
+    ! See A. Balan, G. May and J. Schoberl: "A Stable Spectral Difference Method for Triangles", 2011, Spiteri and Ruuth 2002
     implicit none
     external :: trend_fun
     real(8)  :: dt
     
     real(8), dimension(5,5) :: alpha, beta
 
-    alpha = reshape((/1.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.444370494067_8, 0.555629505933_8, 0.0_8, 0.0_8, 0.0_8,  &
+    alpha = reshape ((/1.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.444370494067_8, 0.555629505933_8, 0.0_8, 0.0_8, 0.0_8,  &
          0.620101851385_8, 0.0_8, 0.379898148615_8, 0.0_8, 0.0_8, 0.178079954108_8, 0.0_8, 0.0_8, 0.821920045892_8, 0.0_8,  &
          0.006833258840_8, 0.0_8, 0.517231672090_8, 0.127598311333_8, 0.348336757737_8/), (/5, 5/))
 
-    beta = reshape((/0.391752227004_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.36841059263_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, &
+    beta = reshape ((/0.391752227004_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.36841059263_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, &
          0.251891774247_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.544974750212_8, 0.0_8, 0.0_8, 0.0_8, 0.0_8, 0.0846041633821_8, &
          0.226007483194_8/), (/5, 5/))
 
@@ -73,28 +73,28 @@ contains
 
   subroutine RK4 (trend_fun, dt)
     ! Low storage four stage second order accurate Runge-Kutta scheme used in Dubos et al (2015) Geosci. Model Dev., 8, 3131–3150, 2015.
-    ! fourth order accurate for linear equations.
+    ! Fourth order accurate for linear equations, stable for CFL <= 2*sqrt(2) ~ 2.83.
     ! Does not require extra solution variables.
     implicit none
     external :: trend_fun
     real(8)  :: dt
   
-    call manage_RK_mem
+    call manage_q1_mem
 
     call trend_fun (sol, trend) 
-    call RK_sub_step1 (sol, trend, 1.0_8, dt/4.0_8, q1)
+    call RK_sub_step (sol, trend, dt/4, q1)
     call WT_after_step (q1, wav_coeff)
 
     call trend_fun (q1, trend) 
-    call RK_sub_step1 (sol, trend, 1.0_8, dt/3.0_8, q1)
+    call RK_sub_step (sol, trend, dt/3, q1)
     call WT_after_step (q1, wav_coeff)
 
     call trend_fun (q1, trend) 
-    call RK_sub_step1 (sol, trend, 1.0_8, dt/2.0_8, q1)
+    call RK_sub_step (sol, trend, dt/2, q1)
     call WT_after_step (q1, wav_coeff)
 
     call trend_fun (q1, trend) 
-    call RK_sub_step1 (sol, trend, 1.0_8, dt, sol)
+    call RK_sub_step (sol, trend, dt, sol)
     call WT_after_step (sol, wav_coeff, level_start-1)
   end subroutine RK4
 
@@ -102,13 +102,11 @@ contains
     ! Euler time step
     ! Stable for CFL<1, first order
     implicit none
-    real(8) :: dt
+    real(8)  :: dt
     external :: trend_fun
-
-    integer :: d, k, v, start
-
+    
     call trend_fun (sol, trend)
-    call RK_sub_step1 (sol, trend, 1.0_8, dt, sol)
+    call RK_sub_step (sol, trend, dt, sol)
     call WT_after_step (sol, wav_coeff, level_start-1)
   end subroutine euler
 
@@ -120,31 +118,50 @@ contains
     call init_comm_mod
     call init_ops_mod
     call init_multi_level_mod
-
-    initialized = .True.
+    initialized = .true.
   end subroutine init_time_integr_mod
+
+  subroutine RK_sub_step (sols, trends, dt, dest)
+    implicit none
+    real(8)                                                              :: dt
+    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels)                :: sols
+    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels)                :: trends
+    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels), intent(inout) :: dest
+    
+    integer :: d, ibeg, iend, k, v
+
+    do d = 1, size(grid)
+       do k = 1, zlevels
+          do v = S_MASS, S_VELO
+             ibeg = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start + 1
+             iend = sols(v,k)%data(d)%length
+             dest(v,k)%data(d)%elts(ibeg:iend) = sols(v,k)%data(d)%elts(ibeg:iend) + dt * trends(v,k)%data(d)%elts(ibeg:iend)
+          end do
+       end do
+    end do
+    dest%bdry_uptodate = .false.
+  end subroutine RK_sub_step
 
   subroutine RK_sub_step1 (sols, trends, alpha, dt, dest)
     implicit none
     real(8) :: alpha, dt
-    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels) :: sols
-    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels) :: trends
+    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels)                :: sols
+    type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels)                :: trends
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels), intent(inout) :: dest
     
-    integer :: k, v, s, t, d, start
+    integer :: k, v, d, start
 
     do k = 1, zlevels
        do d = 1, size(grid)
+          start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
           do v = S_MASS, S_VELO
-             start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
              dest(v,k)%data(d)%elts(start+1:dest(v,k)%data(d)%length) = &
-                  alpha*sols(v,k)%data(d)%elts(start+1:sols(v,k)%data(d)%length) &
-                  + dt*trends(v,k)%data(d)%elts(start+1:trends(v,k)%data(d)%length)
+                  alpha * sols(v,k)%data(d)%elts(start+1:sols(v,k)%data(d)%length) &
+                  + dt * trends(v,k)%data(d)%elts(start+1:trends(v,k)%data(d)%length)
           end do
        end do
        dest(:,k)%bdry_uptodate = .False.
     end do
-
   end subroutine RK_sub_step1
 
   subroutine RK_sub_step2 (sol1, sol2, trends, alpha, dt, dest)
@@ -153,19 +170,19 @@ contains
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels) :: sol1, sol2
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels) :: trends
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels), intent(inout) :: dest
-    integer k, v, s, t, d, start
+    integer k, v, d, start
 
     do k = 1, zlevels
        do d = 1, size(grid)
+          start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
           do v = S_MASS, S_VELO
-             start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
              dest(v,k)%data(d)%elts(start+1:dest(v,k)%data(d)%length) = &
                   alpha(1)*sol1(v,k)%data(d)%elts(start+1:sol1(v,k)%data(d)%length) &
                   + alpha(2)*sol2(v,k)%data(d)%elts(start+1:sol2(v,k)%data(d)%length) &
                   + dt*trends(v,k)%data(d)%elts(start+1:trends(v,k)%data(d)%length)
           end do
        end do
-       dest(:,k)%bdry_uptodate = .False.
+       dest(:,k)%bdry_uptodate = .false.
     end do
 
   end subroutine RK_sub_step2
@@ -178,13 +195,12 @@ contains
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels) :: trend1, trend2
     type(Float_Field), dimension(S_MASS:S_VELO,1:zlevels), intent(inout) :: dest
     
-    integer :: k, v, s, t, d, start
+    integer :: k, v, d, start
 
     do k = 1, zlevels
        do d = 1, size(grid)
+          start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
           do v = S_MASS, S_VELO
-             start = (1+2*(POSIT(v)-1))*grid(d)%patch%elts(2+1)%elts_start ! start of second level
-
              dest(v,k)%data(d)%elts(start+1:dest(v,k)%data(d)%length) = &
                   alpha(1)*sol1(v,k)%data(d)%elts(start+1:sol1(v,k)%data(d)%length) &
                   + alpha(2)*sol2(v,k)%data(d)%elts(start+1:sol2(v,k)%data(d)%length) &
@@ -207,26 +223,38 @@ contains
 
     do k = 1, zlevels
        do v = S_MASS, S_VELO
-          call init_Float_Field(q1(v,k), POSIT(v))
-          call init_Float_Field(q2(v,k), POSIT(v))
-          call init_Float_Field(q3(v,k), POSIT(v))
-          call init_Float_Field(q4(v,k), POSIT(v))
-          call init_Float_Field(dq1(v,k), POSIT(v))
+          call init_Float_Field (q1(v,k),  POSIT(v))
+          call init_Float_Field (q2(v,k),  POSIT(v))
+          call init_Float_Field (q3(v,k),  POSIT(v))
+          call init_Float_Field (q4(v,k),  POSIT(v))
+          call init_Float_Field (dq1(v,k), POSIT(v))
        end do
-    end do
 
-    do k = 1, zlevels
        do d = 1, size(grid)
           do v = S_MASS, S_VELO
-             call init(q1(v,k)%data(d), sol(v,k)%data(d)%length); q1(v,k)%data(d)%elts = dble(3-v)
-             call init(q2(v,k)%data(d), sol(v,k)%data(d)%length); q2(v,k)%data(d)%elts = dble(3-v)
-             call init(q3(v,k)%data(d), sol(v,k)%data(d)%length); q3(v,k)%data(d)%elts = dble(3-v)
-             call init(q4(v,k)%data(d), sol(v,k)%data(d)%length); q4(v,k)%data(d)%elts = dble(3-v)
-             call init(dq1(v,k)%data(d), sol(v,k)%data(d)%length); dq1(v,k)%data(d)%elts = dble(3-v) 
+             call init (q1(v,k)%data(d),  sol(v,k)%data(d)%length);  q1(v,k)%data(d)%elts = dble(3-v)
+             call init (q2(v,k)%data(d),  sol(v,k)%data(d)%length);  q2(v,k)%data(d)%elts = dble(3-v)
+             call init (q3(v,k)%data(d),  sol(v,k)%data(d)%length);  q3(v,k)%data(d)%elts = dble(3-v)
+             call init (q4(v,k)%data(d),  sol(v,k)%data(d)%length);  q4(v,k)%data(d)%elts = dble(3-v)
+             call init (dq1(v,k)%data(d), sol(v,k)%data(d)%length); dq1(v,k)%data(d)%elts = dble(3-v) 
           end do
        end do
     end do
   end subroutine init_RK_mem
+
+  subroutine manage_q1_mem
+    implicit none
+    integer :: d, k, v, n_new
+    
+    do d = 1, size(grid)
+       do k = 1, zlevels
+          do v = S_MASS, S_VELO
+             n_new = sol(v,k)%data(d)%length - q1(v,k)%data(d)%length
+             if (n_new > 0) call extend (q1(v,k)%data(d), n_new, dble(3-v))
+          end do
+       end do
+    end do
+  end subroutine manage_q1_mem
 
   subroutine manage_RK_mem
     implicit none
@@ -237,11 +265,11 @@ contains
           do v = S_MASS, S_VELO
              n_new = sol(v,k)%data(d)%length - q1(v,k)%data(d)%length
              if (n_new > 0) then
-                call extend(q1(v,k)%data(d),  n_new, dble(3-v))
-                call extend(q2(v,k)%data(d),  n_new, dble(3-v))
-                call extend(q3(v,k)%data(d),  n_new, dble(3-v))
-                call extend(q4(v,k)%data(d),  n_new, dble(3-v))
-                call extend(dq1(v,k)%data(d), n_new, dble(3-v))
+                call extend (q1(v,k)%data(d),  n_new, dble(3-v))
+                call extend (q2(v,k)%data(d),  n_new, dble(3-v))
+                call extend (q3(v,k)%data(d),  n_new, dble(3-v))
+                call extend (q4(v,k)%data(d),  n_new, dble(3-v))
+                call extend (dq1(v,k)%data(d), n_new, dble(3-v))
              end if
           end do
        end do
