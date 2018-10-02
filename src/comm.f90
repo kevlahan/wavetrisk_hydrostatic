dd1b6cdc7d37464f29898b884b227c82817bad39
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-10-02 15:30:28 +0200

    Revert to stop (finalize+stop does not actually stop execution).

--- src/comm.f90
+++ src/comm.f90
@@ -977,7 +977,6 @@ contains
           mu = sol(S_MASS,k)%data(d)%elts(id_i)
           if (mu < 0.0_8) then
              write (6,'(A,es10.4,i3)') "ERROR: mass = ",  mu, " is negative at level ", k
-             call finalize
              stop
           end if
           col_mass = col_mass + mu
