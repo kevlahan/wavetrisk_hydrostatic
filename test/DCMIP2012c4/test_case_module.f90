acc26deea973a6aeaf75d56ec031c13ecd3e5ea5
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-10-18 10:35:08 +0200

    Correct Laplace order for setting viscosity.

--- test/DCMIP2012c4/test_case_module.f90
+++ test/DCMIP2012c4/test_case_module.f90
@@ -408,10 +408,10 @@ contains
     elseif (Laplace_order_init == 1 .or. Laplace_order_init == 2) then
        L_scaled = L_diffusion / 2**(max_level-min_level) ! Correct length scales for finest grid
 
-       viscosity_mass = L_scaled(1)**(2*Laplace_order) / dt_cfl * C_diffusion * n_diffuse
-       viscosity_temp = L_scaled(1)**(2*Laplace_order) / dt_cfl * C_diffusion * n_diffuse
-       viscosity_divu = L_scaled(2)**(2*Laplace_order) / dt_cfl * C_diffusion * n_diffuse
-       viscosity_rotu = L_scaled(3)**(2*Laplace_order) / dt_cfl * C_diffusion * n_diffuse
+       viscosity_mass = L_scaled(1)**(2*Laplace_order_init) / dt_cfl * C_diffusion * n_diffuse
+       viscosity_temp = L_scaled(1)**(2*Laplace_order_init) / dt_cfl * C_diffusion * n_diffuse
+       viscosity_divu = L_scaled(2)**(2*Laplace_order_init) / dt_cfl * C_diffusion * n_diffuse
+       viscosity_rotu = L_scaled(3)**(2*Laplace_order_init) / dt_cfl * C_diffusion * n_diffuse
     elseif (Laplace_order_init > 2) then
        if (rank == 0) write (6,'(A)') 'Unsupported iterated Laplacian (only 0, 1 or 2 supported)'
        stop
