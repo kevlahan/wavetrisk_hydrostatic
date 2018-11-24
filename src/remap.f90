f29b067b57383495ac9aabea6b9462271d2873be
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-11-24 16:47:12 +0100

    Combine velocity remaps into one.  Still gives HS jets too high for small numbers of levels.

--- src/remap.f90
+++ src/remap.f90
@@ -14,9 +14,7 @@ contains
     ! Remap
     do l = level_start, level_end
        call apply_onescale (remap_scalars, l, z_null, 0, 1)
-       call apply_onescale (remap_velo_RT, l, z_null, 0, 0)
-       call apply_onescale (remap_velo_DG, l, z_null, 0, 0)
-       call apply_onescale (remap_velo_UP, l, z_null, 0, 0)
+       call apply_onescale (remap_velo,    l, z_null, 0, 0)
     end do
     sol%bdry_uptodate = .false.
     call update_array_bdry (sol, NONE)
@@ -88,14 +86,16 @@ contains
     end do
   end subroutine remap_scalars
 
-  subroutine remap_velo_RT (dom, i, j, z_null, offs, dims)
+  subroutine remap_velo (dom, i, j, z_null, offs, dims)
     ! Remap RT velocity onto original vertical grid by linear interpolation of mass flux
     type (Domain)                   :: dom
     integer                         :: i, j, z_null
     integer, dimension (N_BDRY+1)   :: offs
     integer, dimension (2,N_BDRY+1) :: dims
 
-    integer                       :: d, e, id, id_e, id_i, idE_i, k, zlev
+    integer                       :: d, e, id, id_i, idE_i, k, zlev
+    integer, dimension(1:EDGE)    :: id_e, id_r
+    
     real(8)                       :: mass_e, X_e
     real(8), dimension(zlevels)   :: massflux
     real(8), dimension(zlevels+1) :: massflux_cumul, new_massflux_cumul
@@ -103,117 +103,37 @@ contains
     d     = dom%id + 1
     id    = idx (i, j, offs, dims)
     id_i  = id + 1
-    id_e  = EDGE*id + RT + 1
-    idE_i = idx(i+1, j,   offs, dims) + 1
-    
+
+    id_r(RT+1) = idx(i+1, j,   offs, dims) + 1
+    id_r(DG+1) = idx(i+1, j+1, offs, dims) + 1
+    id_r(UP+1) = idx(i,   j+1, offs, dims) + 1
+
+    do e = 1, EDGE
+       id_e(e) = EDGE*id + e
+    end do
+
     ! Do not remap inactive nodes
-    if (dom%mask_n%elts(id_i) < TRSK .or. dom%mask_e%elts(id_e) < TRSK) return 
-
-    massflux_cumul(1) = 0.0_8
-    do k = 1, zlevels
-       mass_e = interp (trend(S_MASS,k)%data(d)%elts(id_i), trend(S_MASS,k)%data(d)%elts(idE_i))
-       massflux(k) = sol(S_VELO,k)%data(d)%elts(id_e) * mass_e 
-       massflux_cumul(k+1) = massflux_cumul(k) + massflux(k)
-    end do
-      
-    do k = 1, zlevels+1
-       X_e = interp (exner_fun(k)%data(d)%elts(id_i), exner_fun(k)%data(d)%elts(idE_i))
-       do e = 1, EDGE
-          zlev = min (zlevels, floor (X_e)) ; if (zlev < 1) return
-          X_e = X_e - zlev
+    if (dom%mask_n%elts(id_i) < TRSK .or. minval (dom%mask_e%elts(id_e)) < TRSK) return 
+    
+    do e = 1, EDGE
+       massflux_cumul(1) = 0.0_8
+       do k = 1, zlevels
+          mass_e = interp (trend(S_MASS,k)%data(d)%elts(id_i), trend(S_MASS,k)%data(d)%elts(id_r(e)))
+          massflux(k) = sol(S_VELO,k)%data(d)%elts(id_e(e)) * mass_e 
+          massflux_cumul(k+1) = massflux_cumul(k) + massflux(k)
        end do
-       new_massflux_cumul(k) = massflux_cumul(zlev) + X_e * massflux(zlev)
-    end do
-    
-    do k = 1, zlevels
-       mass_e = interp (sol(S_MASS,k)%data(d)%elts(id_i), sol(S_MASS,k)%data(d)%elts(idE_i))
-       sol(S_VELO,k)%data(d)%elts(id_e) = (new_massflux_cumul(k+1) - new_massflux_cumul(k)) / mass_e
-    end do
-  end subroutine remap_velo_RT
 
-   subroutine remap_velo_DG (dom, i, j, z_null, offs, dims)
-    ! Remap velocity onto original vertical grid by linear interpolation of mass flux
-    type (Domain)                   :: dom
-    integer                         :: i, j, z_null
-    integer, dimension (N_BDRY+1)   :: offs
-    integer, dimension (2,N_BDRY+1) :: dims
-
-    integer                       :: d, e, id, id_e, id_i, idNE_i, k, zlev
-    real(8)                       :: mass_e, X_e
-    real(8), dimension(zlevels)   :: massflux
-    real(8), dimension(zlevels+1) :: massflux_cumul, new_massflux_cumul
-
-    d      = dom%id + 1
-    id     = idx (i, j, offs, dims)
-    id_i   = id + 1
-    id_e   = EDGE*id + DG + 1
-    idNE_i = idx (i+1, j+1, offs, dims) + 1
-    
-    ! Do not remap inactive nodes
-    if (dom%mask_n%elts(id_i) < TRSK .or. dom%mask_e%elts(id_e) < TRSK) return 
-
-    massflux_cumul(1) = 0.0_8
-    do k = 1, zlevels
-       mass_e = interp (trend(S_MASS,k)%data(d)%elts(id_i), trend(S_MASS,k)%data(d)%elts(idNE_i))
-       massflux(k) = sol(S_VELO,k)%data(d)%elts(id_e) * mass_e 
-       massflux_cumul(k+1) = massflux_cumul(k) + massflux(k)
-    end do
-      
-    do k = 1, zlevels+1
-       X_e = interp (exner_fun(k)%data(d)%elts(id_i), exner_fun(k)%data(d)%elts(idNE_i))
-       do e = 1, EDGE
-          zlev = min (zlevels, floor (X_e)) ; if (zlev < 1) return
+       do k = 1, zlevels+1
+          X_e = interp (exner_fun(k)%data(d)%elts(id_i), exner_fun(k)%data(d)%elts(id_r(e)))
+          zlev = min (zlevels, floor (X_e)); if (zlev < 1) return
           X_e = X_e - zlev
+          new_massflux_cumul(k) = massflux_cumul(zlev) + X_e * massflux(zlev)
        end do
-       new_massflux_cumul(k) = massflux_cumul(zlev) + X_e * massflux(zlev)
-    end do
-    
-    do k = 1, zlevels
-       mass_e = interp (sol(S_MASS,k)%data(d)%elts(id_i), sol(S_MASS,k)%data(d)%elts(idNE_i))
-       sol(S_VELO,k)%data(d)%elts(id_e) = (new_massflux_cumul(k+1) - new_massflux_cumul(k)) / mass_e
-    end do
-  end subroutine remap_velo_DG
 
-   subroutine remap_velo_UP (dom, i, j, z_null, offs, dims)
-    ! Remap UP velocity onto original vertical grid by linear interpolation of mass flux
-    type (Domain)                   :: dom
-    integer                         :: i, j, z_null
-    integer, dimension (N_BDRY+1)   :: offs
-    integer, dimension (2,N_BDRY+1) :: dims
-
-    integer                       :: d, e, id, id_e, id_i, idN_i, k, zlev
-    real(8)                       :: mass_e, X_e
-    real(8), dimension(zlevels)   :: massflux
-    real(8), dimension(zlevels+1) :: massflux_cumul, new_massflux_cumul
-
-    d     = dom%id + 1
-    id    = idx (i, j, offs, dims)
-    id_i  = id + 1
-    id_e  = EDGE*id + UP + 1
-    idN_i = idx (i, j+1, offs, dims) + 1
-    
-    ! Do not remap inactive nodes
-    if (dom%mask_n%elts(id_i) < TRSK .or. dom%mask_e%elts(id_e) < TRSK) return 
-
-    massflux_cumul(1) = 0.0_8
-    do k = 1, zlevels
-       mass_e = interp (trend(S_MASS,k)%data(d)%elts(id_i), trend(S_MASS,k)%data(d)%elts(idN_i))
-       massflux(k) = sol(S_VELO,k)%data(d)%elts(id_e) * mass_e 
-       massflux_cumul(k+1) = massflux_cumul(k) + massflux(k)
-    end do
-      
-    do k = 1, zlevels+1
-       X_e = interp (exner_fun(k)%data(d)%elts(id_i), exner_fun(k)%data(d)%elts(idN_i))
-       do e = 1, EDGE
-          zlev = min (zlevels, floor (X_e)) ; if (zlev < 1) return
-          X_e = X_e - zlev
+       do k = 1, zlevels
+          mass_e = interp (sol(S_MASS,k)%data(d)%elts(id_i), sol(S_MASS,k)%data(d)%elts(id_r(e)))
+          sol(S_VELO,k)%data(d)%elts(id_e(e)) = (new_massflux_cumul(k+1) - new_massflux_cumul(k)) / mass_e
        end do
-       new_massflux_cumul(k) = massflux_cumul(zlev) + X_e * massflux(zlev)
     end do
-    
-    do k = 1, zlevels
-       mass_e = interp (sol(S_MASS,k)%data(d)%elts(id_i), sol(S_MASS,k)%data(d)%elts(idN_i))
-       sol(S_VELO,k)%data(d)%elts(id_e) = (new_massflux_cumul(k+1) - new_massflux_cumul(k)) / mass_e
-    end do
-  end subroutine remap_velo_UP
+  end subroutine remap_velo
  end module remap_mod
