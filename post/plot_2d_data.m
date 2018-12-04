ea6f67efeed9261e507d2e6238cdac73c44489fb
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-12-04 15:01:39 +0100

    Rearrange order of routines.

--- post/plot_2d_data.m
+++ post/plot_2d_data.m
@@ -2,10 +2,10 @@
 %test_case = 'DCMIP2008c5'; run_id = 'DCMIP2008c5'; run_dir = '';
 %test_case = 'DCMIP2012c4'; run_id = 'DCMIP2012c4_J7'; run_dir = '';
 %test_case = 'DCMIP2012c4'; run_id = 'DCMIP2012c4'; run_dir = 'test1/';
-test_case = 'Held_Suarez'; run_id = 'Held_Suarez_J5'; run_dir = '';
+test_case = 'Held_Suarez'; run_id = 'Held_Suarez_J4'; run_dir = '';
 
 % 2d projection options: 'temp' 'zonal' 'merid' 'geopot' 'vort' 'surf_press' 'ke' 'temp_var' 'eddy_mom' 'eddy_ke' 'eddy_heat_flux'
-itype     = 'zonal';  % field to plot
+itype     = 'eddy_heat_flux';  % field to plot
 lon_lat   = false;  % Plot longitude - latitude data
 zonal_avg = true;   % Plot zonally averaged data
 shift     = true;   % shift left boundary to zero longitude
@@ -32,69 +32,6 @@ elseif (strcmp(machine,'mac'))
 end
 
 set(0,'defaulttextinterpreter','latex')
-%% Log data plots
-% Load log file
-%figure;
-
-beg = 1;
-%beg = 47625; % Held-Suarez
-
-% Number of dof on equivalent uniform grid
-Nunif = 4 * 10*4^Jmax;
-
-% Total number of degrees of freedom over all scales (this is what is
-% counted)
-Nmax = 4 * 10*4^Jmin;
-for j=Jmin+1:Jmax
-    Nmax = Nmax + 4 * 10*4^j;
-end
-
-log_data = load([pathid run_id '_log']);
-day = 24;
-if ilog == cpudof
-    plot(log_data(beg:end,1)/day,log_data(beg:end,cpu)./log_data(beg:end,dof),'k-','linewidth',1.5);
-elseif ilog == compression
-    plot(log_data(beg:end,1)/day,Nmax./log_data(beg:end,dof),'k-','linewidth',1.5);
-else
-    plot(log_data(beg:end,1)/day,log_data(beg:end,ilog),'r-','linewidth',1.5);
-end
-
-if ilog == dt
-    ylab = '$\Delta t$';
-elseif ilog == tol_mass
-    ylab = 'Mass threshold';
-elseif ilog == tol_temp
-    ylab = 'Temperature threshold';
-elseif ilog == tol_velo
-    ylab = 'Velocity threshold';
-elseif ilog == J
-    ylab = 'J';
-elseif ilog == dof
-    ylab = '${\cal N}$ (active nodes and edges)';
-elseif ilog == min_mass
-    ylab = 'Minimum Mass';
-elseif ilog == mass_err
-    ylab = 'Mass error';
-elseif ilog == balance
-    ylab = 'Load balance';
-elseif ilog == cpu
-    ylab = 'CPU time / $\Delta t$ (s)';
-    
-elseif ilog == cpudof
-    ylab = 'CPU time / ${\cal N}$';
-elseif ilog == compression
-    ylab = 'Compression ratio';
-end
-
-if ilog < cpudof
-    axis([0 log_data(end,1)/day 0 max(log_data(beg:end,ilog))]);
-elseif ilog == cpudof
-    axis([0 log_data(end,1)/day 0 max(log_data(beg:end,cpu)./log_data(beg:end,dof))]);
-elseif ilog == compression
-    axis([0 log_data(end,1)/day 1 1*max(Nmax./log_data(beg:end,dof))]);
-end
-xlabel('Time (days)');ylabel(ylab);grid on;
-set(findall(gcf,'-property','FontSize'),'FontSize',22)
 %% Uncompress data files for 2d plots
 
 % Extract files
@@ -143,7 +80,7 @@ if (strcmp(itype,'temp')) % Plot temperature
         s_zo = s_zo + load([file_base '11']);
     end
 elseif (strcmp(itype,'temp_var')) % Plot temperature variance
-    c_scale = 0:2:40; % Held-Suarez
+    c_scale = 0:5:40; % Held-Suarez
     v_title = 'Temperature variance (K^2)';
     s_zo = s_zo + load([file_base '12']);
 elseif (strcmp(itype,'zonal')) % Plot zonal velocity data
@@ -209,19 +146,19 @@ elseif (strcmp(itype,'ke')) % Plot zonal kinetic energy
     v_title = 'Kinetic energy (m^2/s^2)';
     s_zo = s_zo+load([file_base '15']);
 elseif (strcmp(itype,'eddy_mom')) % Plot zonal eddy momentum flux
-    c_scale = -100:10:100; % Held-Suarez
+    c_scale = -100:20:100; % Held-Suarez
     v_title = 'Eddy momentum flux (m^2/s^2)';
     s_zo = s_zo+load([file_base '16']);
 elseif (strcmp(itype,'eddy_ke')) % Plot zonal eddy kinetic energy
     if (strcmp(test_case,'DCMIP2008c5'))
         c_scale = 0:5:100;
     elseif (strcmp(test_case,'Held_Suarez'))
-        c_scale = 0:20:480;
+        c_scale = 0:40:480;
     end
     v_title = 'Eddy kinetic energy (m^2/s^2)';
     s_zo = s_zo+load([file_base '17']);
 elseif (strcmp(itype,'eddy_heat_flux')) % Plot zonal eddy heat flux
-    c_scale = -20:2:20; % Held-Suarez
+    c_scale = -20:5:20; % Held-Suarez
     v_title = 'Eddy kinetic energy (K m/s)';
     s_zo = s_zo+load([file_base '18']);
 end
@@ -237,8 +174,71 @@ end
 if (zonal_avg)
     fprintf('Minimum value of variable %s = %8.4e\n', itype, min(min(s_zo)));
     fprintf('Maximum value of variable %s = %8.4e\n', itype, max(max(s_zo)));
-    plot_zonal_avg_data(s_zo, lat, P_z, c_scale, v_title, smooth, lines, 0)
+    plot_zonal_avg_data(s_zo, lat, P_z, c_scale, v_title, smooth, lines, 1)
 end
 % Erase extracted files
 file_erase = ['\rm ' file_base '*'];
 system(file_erase);
+%% Log data plots
+% Load log file
+%figure;
+
+beg = 1;
+%beg = 47625; % Held-Suarez
+
+% Number of dof on equivalent uniform grid
+Nunif = 4 * 10*4^Jmax;
+
+% Total number of degrees of freedom over all scales (this is what is
+% counted)
+Nmax = 4 * 10*4^Jmin;
+for j=Jmin+1:Jmax
+    Nmax = Nmax + 4 * 10*4^j;
+end
+
+log_data = load([pathid run_id '_log']);
+day = 24;
+if ilog == cpudof
+    plot(log_data(beg:end,1)/day,log_data(beg:end,cpu)./log_data(beg:end,dof),'k-','linewidth',1.5);
+elseif ilog == compression
+    plot(log_data(beg:end,1)/day,Nmax./log_data(beg:end,dof),'k-','linewidth',1.5);
+else
+    plot(log_data(beg:end,1)/day,log_data(beg:end,ilog),'r-','linewidth',1.5);
+end
+
+if ilog == dt
+    ylab = '$\Delta t$';
+elseif ilog == tol_mass
+    ylab = 'Mass threshold';
+elseif ilog == tol_temp
+    ylab = 'Temperature threshold';
+elseif ilog == tol_velo
+    ylab = 'Velocity threshold';
+elseif ilog == J
+    ylab = 'J';
+elseif ilog == dof
+    ylab = '${\cal N}$ (active nodes and edges)';
+elseif ilog == min_mass
+    ylab = 'Minimum Mass';
+elseif ilog == mass_err
+    ylab = 'Mass error';
+elseif ilog == balance
+    ylab = 'Load balance';
+elseif ilog == cpu
+    ylab = 'CPU time / $\Delta t$ (s)';
+    
+elseif ilog == cpudof
+    ylab = 'CPU time / ${\cal N}$';
+elseif ilog == compression
+    ylab = 'Compression ratio';
+end
+
+if ilog < cpudof
+    axis([0 log_data(end,1)/day 0 max(log_data(beg:end,ilog))]);
+elseif ilog == cpudof
+    axis([0 log_data(end,1)/day 0 max(log_data(beg:end,cpu)./log_data(beg:end,dof))]);
+elseif ilog == compression
+    axis([0 log_data(end,1)/day 1 1*max(Nmax./log_data(beg:end,dof))]);
+end
+xlabel('Time (days)');ylabel(ylab);grid on;
+set(findall(gcf,'-property','FontSize'),'FontSize',22)
