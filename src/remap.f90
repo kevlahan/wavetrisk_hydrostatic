070b0e04385907910824d29b0009cebdb3a7bb86
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2018-12-14 17:06:34 +0100

    Cleanup and switch to remap1 (no enhance) as default.

--- src/remap.f90
+++ src/remap.f90
@@ -16,6 +16,8 @@ contains
   subroutine remap_vertical_coordinates
     ! Remap the Lagrangian layers to initial vertical grid given a_vert and b_vert vertical coordinate parameters 
     ! Conserves mass, potential temperature and velocity divergence
+    ! remap0 is too diffusive, remap1, remap2W are very stable and remap2PPM, remap2S, remap4 are less stable.
+    ! remap1 is usually the best choice.
     implicit none
     integer            :: l
     logical, parameter :: standard = .true. ! .false. uses Lin (2004) scheme (!! unstable for Held-Suarez!!)
@@ -23,15 +25,15 @@ contains
     ! Choose interpolation method:
     ! [these methods are modified from routines provided by Alexander Shchepetkin (IGPP, UCLA)]
     !
-    ! remap0         = piecewise-constant reconstruction (checks that grid is not changing too fast)
-    ! remap1_tile    = piecewise-linear reconstruction with van Leer limiter to ensure monotonicity
-    ! remap2PPM_tile = reconstruction by PPM code of Colella and Woodward (1984) (could have higher errors at boundaries)
-    ! remap2S        = basic parabolic spline reconstruction
-    ! remap2W        = parabolic WENO reconstruction
-    ! remap4_tile    = parabolic WENO reconstruction enhanced by quartic power-law reconciliation step
+    ! remap0    = piecewise-constant reconstruction (checks that grid is not changing too fast)
+    ! remap1    = piecewise-linear reconstruction with van Leer limiter to ensure monotonicity
+    ! remap2PPM = reconstruction by PPM code of Colella and Woodward (1984) 
+    ! remap2S   = basic parabolic spline reconstruction
+    ! remap2W   = parabolic WENO reconstruction
+    ! remap4    = parabolic WENO reconstruction enhanced by quartic power-law reconciliation step
     !                  (ensures continuity of both value and first derivative at each interface)
-    interp_scalar => remap4_tile
-    interp_velo   => remap4_tile
+    interp_scalar => remap1
+    interp_velo   => remap1
 
     ! Current surface pressure
     call cal_surf_press (sol)
@@ -278,7 +280,7 @@ contains
     implicit none
     integer                 :: N
     real(8), dimension(1:N) :: var_new, var_old
-    real(8), dimension(0:N) :: z_new,  z_old
+    real(8), dimension(0:N) :: z_new, z_old
     
     integer                 :: ierr, k
     real(8)                 :: dz
@@ -310,7 +312,7 @@ contains
     if (ierr /= 0 .and. rank == 0)  write (6,'(A)') 'ERROR: Grid is changing too fast.'
   end subroutine remap0
 
-  subroutine remap1_tile (N, var_new, z_new, var_old, z_old)
+  subroutine remap1 (N, var_new, z_new, var_old, z_old)
     !
     ! Remapping procedure using piecewise-linear reconstruction: 
     !---------- --------- ----- --------- ------ ---------------
@@ -325,15 +327,15 @@ contains
     implicit none
     integer                 :: N
     real(8), dimension(1:N) :: var_new, var_old
-    real(8), dimension(0:N) :: z_new,  z_old
+    real(8), dimension(0:N) :: z_new, z_old
 
     integer                 :: k, iter
     real(8)                 :: cff, cff1, dh, dL, dR, dz
     real(8), parameter      :: Zero=0.0_8, Half=0.5_8
     real(8), dimension(0:N) :: aL, aR, FC
     real(8), dimension(1:N) :: Hz
-    logical, parameter      :: NEUMANN = .false. ! commented out
-    logical, parameter      :: ENHANCE = .true.  ! commented in
+    logical, parameter      :: NEUMANN = .true.   ! commented in (zero slope at boundaries)
+    logical, parameter      :: ENHANCE = .false.  ! commented out
 
     do k = 1, N
        Hz(k) = z_old(k) - z_old(k-1)
@@ -344,12 +346,12 @@ contains
     end do
 
     ! if (NEUMANN) then
-    !    FC(0) = Zero
-    !    FC(N) = Zero
+    FC(0) = Zero
+    FC(N) = Zero
     ! else
-    FC(0) = FC(1)
-    FC(N) = FC(N-1)
-    !end if
+    !    FC(0) = FC(1)
+    !    FC(N) = FC(N-1)
+    ! end if
 
     do k = 1, N
        if (FC(k)*FC(k-1) < Zero) then
@@ -360,29 +362,29 @@ contains
        aR(k) = var_old(k) + cff*Hz(k)
        aL(k) = var_old(k) - cff*Hz(k)
     end do     !--> discard FC
-
-    !if (ENHANCE) then
-       do iter = 1, 10                        ! Reconcile side 
-          do k = 1, N-1                       ! values at each interface, then 
-             FC(k) = Half * (aR(k) + aL(k+1)) ! use minmod limiter again to maintain monotonicity
-          end do
-          FC(N) = aR(N)
-          FC(0) = aL(1)
-          do k = 1, N
-             dR = FC(k) - var_old(k)
-             dL = var_old(k) - FC(k-1)
-             if (dR*dL < Zero) then
-                cff = Zero
-             elseif (abs(dR) > abs(dL)) then
-                cff = dL
-             else
-                cff = dR
-             end if
-             aR(k) = var_old(k) + cff
-             aL(k) = var_old(k) - cff
-          end do
-       end do
-    !end if
+    
+    ! if (ENHANCE) then
+    !    do iter = 1, 10                        ! Reconcile side 
+    !       do k = 1, N-1                       ! values at each interface, then 
+    !          FC(k) = Half * (aR(k) + aL(k+1)) ! use minmod limiter again to maintain monotonicity
+    !       end do
+    !       FC(N) = aR(N)
+    !       FC(0) = aL(1)
+    !       do k = 1, N
+    !          dR = FC(k) - var_old(k)
+    !          dL = var_old(k) - FC(k-1)
+    !          if (dR*dL < Zero) then
+    !             cff = Zero
+    !          elseif (abs(dR) > abs(dL)) then
+    !             cff = dL
+    !          else
+    !             cff = dR
+    !          end if
+    !          aR(k) = var_old(k) + cff
+    !          aL(k) = var_old(k) - cff
+    !       end do
+    !    end do
+    ! end if
 
     ! Remapping: compute
     do k = 1, N-1                          ! finite volume fluxes
@@ -404,16 +406,16 @@ contains
     do k = 1, N
        var_new(k) = (Hz(k)*var_old(k) + FC(k)-FC(k-1)) / (z_new(k)-z_new(k-1))
     end do
-  end subroutine remap1_tile
+  end subroutine remap1
 
-  subroutine remap2PPM_tile (N, var_new, z_new, var_old, z_old)
+  subroutine remap2PPM (N, var_new, z_new, var_old, z_old)
     !
     ! Reconstruction by PPM code of Colella--Woodward, 1984.
     !
     implicit none
     integer                 :: N
     real(8), dimension(1:N) :: var_new, var_old
-    real(8), dimension(0:N) :: z_new,  z_old
+    real(8), dimension(0:N) :: z_new, z_old
 
     integer                 :: k, k1, k2
     real(8)                 :: alpha, cff, cffL, cffR, dL, dR, dz
@@ -422,7 +424,7 @@ contains
     real(8), dimension(0:N) :: aL, aR, CF, FC, FC1
     logical, parameter      :: LIMIT_INTERIOR = .false. ! commented out
     logical, parameter      :: LIMIT_SLOPES   = .true.  ! commented in
-    logical, parameter      :: NEUMANN        = .false. ! commented in
+    logical, parameter      :: NEUMANN        = .true.  ! commented in
 
     do k = 1, N
        Hz(k) = z_old(k) - z_old(k-1)
@@ -433,18 +435,18 @@ contains
        CF(k) = cff * (var_old(k+1)*Hz(k) + var_old(k)*Hz(k+1))
        FC(k) = cff * (var_old(k+1) - var_old(k))
     enddo
-
+    
     do k = 2, N-1
        cff = Hz(k) * ((Two*Hz(k-1) + Hz(k))*FC(k) + (Two*Hz(k+1) + Hz(k))*FC(k-1)) / (Hz(k-1) + Hz(k) + Hz(k+1))
        ! if (LIMIT_SLOPES) then
-          cffR = Two * (var_old(k+1) - var_old(k))
-          cffL = Two * (var_old(k) - var_old(k-1))
-          if (cffR*cffL > Zero) then
-             if (abs(cffL) < abs(cff)) cff = cffL
-             if (abs(cffR) < abs(cff)) cff = cffR
-          else
-             cff = Zero
-          end if
+       cffR = Two * (var_old(k+1) - var_old(k))
+       cffL = Two * (var_old(k) - var_old(k-1))
+       if (cffR*cffL > Zero) then
+          if (abs(cffL) < abs(cff)) cff = cffL
+          if (abs(cffR) < abs(cff)) cff = cffR
+       else
+          cff = Zero
+       end if
        ! end if
        FC1(k) = cff
     end do
@@ -463,26 +465,26 @@ contains
     end do
 
     ! if (NEUMANN) then
-    !    aR(N) = ThreeHalfth*var_old(N) - Half*aL(N)
-    !    aL(1) = ThreeHalfth*var_old(1) - Half*aR(1)
+    aR(N) = ThreeHalfth*var_old(N) - Half*aL(N)
+    aL(1) = ThreeHalfth*var_old(1) - Half*aR(1)
     ! else
-    aR(N) = Two*var_old(N) - aL(N)
-    aL(1) = Two*var_old(1) - aR(1)
+    ! aR(N) = Two*var_old(N) - aL(N)
+    ! aL(1) = Two*var_old(1) - aR(1)
     ! end if
     
     ! if (LIMIT_INTERIOR) then
-    ! do k = 1, N
-    !    dR = aR(k) - var_old(k)
-    !    dL = var_old(k) - aL(k)
-    !    if (dR*dL < Zero) then
-    !       dR = Zero
-    !       dL = Zero
-    !    end if
-    !    if (abs(dR) > Two*abs(dL)) dR = Two * dL
-    !    if (abs(dL) > Two*abs(dR)) dL = Two * dR
-    !    aR(k) = var_old(k) + dR
-    !    aL(k) = var_old(k) - dL
-    ! enddo
+    !    do k = 1, N
+    !       dR = aR(k) - var_old(k)
+    !       dL = var_old(k) - aL(k)
+    !       if (dR*dL < Zero) then
+    !          dR = Zero
+    !          dL = Zero
+    !       end if
+    !       if (abs(dR) > Two*abs(dL)) dR = Two * dL
+    !       if (abs(dL) > Two*abs(dR)) dL = Two * dR
+    !       aR(k) = var_old(k) + dR
+    !       aL(k) = var_old(k) - dL
+    !    end do
     ! end if
     
     !
@@ -519,7 +521,7 @@ contains
     do k = 1, N
        var_new(k) = (Hz(k)*var_old(k) + FC(k)-FC(k-1)) / (z_new(k) - z_new(k-1))
     end do
-  end subroutine remap2PPM_tile
+  end subroutine remap2PPM
 
   subroutine remap2S (N, var_new, z_new, var_old, z_old)
     !
@@ -621,16 +623,15 @@ contains
     implicit none
     integer                 :: N
     real(8), dimension(1:N) :: var_new, var_old
-    real(8), dimension(0:N) :: z_new,  z_old
+    real(8), dimension(0:N) :: z_new, z_old
 
     integer                 :: k
     real(8)                 :: alpha, cff, cffL, cffR, deltaL, deltaR, dz
     real(8), parameter      :: Zero=0.0_8, Half=0.5_8, One=1.0_8, ThreeHalfth=1.5_8, Two=2.0_8, Three=3.0_8, eps = 1.0d-8
     real(8), dimension(1:N) :: Hz
     real(8), dimension(0:N) :: aL, aR, dL, dR, FC, r
-    logical, parameter      :: LIMIT_INTERIOR = .false. ! commented out (setting true could cause oscillations at boundary)
-    logical, parameter      :: LIMIT_SLOPES   = .true.  ! commented in
-    logical, parameter      :: NEUMANN        = .false. ! commented out
+    logical, parameter      :: LIMIT_INTERIOR = .true. ! commented in 
+    logical, parameter      :: NEUMANN        = .true. ! commented out
 
     do k = 1, N
        Hz(k) = z_old(k) - z_old(k-1)
@@ -666,32 +667,32 @@ contains
     end do
 
     ! if (LIMIT_INTERIOR) then
-    !    aR(N) = var_old(N)     ! Boundary conditions for strictly monotonic option: The only way to
-    !    aL(N) = var_old(N)     ! avoid extrapolation toward the
-    !    dR(N) = Zero           ! boundary is to assume that field 
-    !    dL(N) = Zero           ! is simply constant within topmost 
-    !    aR(1) = var_old(1)     ! and bottommost grid boxes. Note 
-    !    aL(1) = var_old(1)     ! that even for NEUMANN boundary 
-    !    dR(1) = Zero           ! conditions, the extrapolated 
-    !    dL(1) = Zero           ! values aR(N) and aL(0) exceed corresponding grid box values.
+    aR(N) = var_old(N)     ! Boundary conditions for strictly monotonic option: The only way to
+    aL(N) = var_old(N)     ! avoid extrapolation toward the
+    dR(N) = Zero           ! boundary is to assume that field 
+    dL(N) = Zero           ! is simply constant within topmost 
+    aR(1) = var_old(1)     ! and bottommost grid boxes. Note 
+    aL(1) = var_old(1)     ! that even for NEUMANN boundary 
+    dR(1) = Zero           ! conditions, the extrapolated 
+    dL(1) = Zero           ! values aR(N) and aL(0) exceed corresponding grid box values.
     ! else 
-    aL(N) = aR(N-1)
-    ! if (NEUMANN) then
-    !    aR(N) = ThreeHalfth*var_old(N) - Half*aL(N)
-    ! else
-    aR(N) = Two*var_old(N) - aL(N)
-    ! end if
-    dR(N) = (Two*aR(N) + aL(N) - Three*var_old(N))**2
-    dL(N) = (Three*var_old(N) - Two*aL(N) - aR(N))**2
+    !    aL(N) = aR(N-1)
+    !    if (NEUMANN) then
+    !       aR(N) = ThreeHalfth*var_old(N) - Half*aL(N)
+    !    else
+    !       aR(N) = Two*var_old(N) - aL(N)
+    !    end if
+    !    dR(N) = (Two*aR(N) + aL(N) - Three*var_old(N))**2
+    !    dL(N) = (Three*var_old(N) - Two*aL(N) - aR(N))**2
 
-    aR(1)=aL(2)
-    if (NEUMANN) then
-       aL(1) = ThreeHalfth*var_old(1) - Half*aR(1)
-    else
-       aL(1) = Two*var_old(1) - aR(1)
-    end if
-    dR(1) = (Two*aR(1) + aL(1) - Three*var_old(1))**2
-    dL(1) = (Three*var_old(1) - Two*aL(1) - aR(1))**2
+    !    aR(1)=aL(2)
+    !    if (NEUMANN) then
+    !       aL(1) = ThreeHalfth*var_old(1) - Half*aR(1)
+    !    else
+    !       aL(1) = Two*var_old(1) - aR(1)
+    !    end if
+    !    dR(1) = (Two*aR(1) + aL(1) - Three*var_old(1))**2
+    !    dL(1) = (Three*var_old(1) - Two*aL(1) - aR(1))**2
     ! end if
 
     ! Reconcile interfacial values aR, aL using WENO 
@@ -716,23 +717,23 @@ contains
     !
     do k = 1, N
        ! if (LIMIT_INTERIOR) then               ! Constrain parabolic
-       !    deltaR = r(k) - var_old(k)          ! segment monotonicity
-       !    deltaL = var_old(k) - r(k-1)        ! like in PPM 
-       !    cffR = Two * deltaR
-       !    cffL = Two * deltaL
-       !    if (deltaR*deltaL < Zero) then
-       !       deltaR = Zero
-       !       deltaL = Zero
-       !    elseif (abs(deltaR) > abs(cffL)) then
-       !       deltaR = cffL
-       !    elseif (abs(deltaL) > abs(cffR)) then
-       !       deltaL = cffR
-       !    endif
-       !    aR(k) = var_old(k) + deltaR
-       !    aL(k) = var_old(k) - deltaL
+       deltaR = r(k) - var_old(k)          ! segment monotonicity
+       deltaL = var_old(k) - r(k-1)        ! like in PPM 
+       cffR = Two * deltaR
+       cffL = Two * deltaL
+       if (deltaR*deltaL < Zero) then
+          deltaR = Zero
+          deltaL = Zero
+       else if (abs(deltaR) > abs(cffL)) then
+          deltaR = cffL
+       else if (abs(deltaL) > abs(cffR)) then
+          deltaL = cffR
+       endif
+       aR(k) = var_old(k) + deltaR
+       aL(k) = var_old(k) - deltaL
        ! else
-       aR(k) = r(k)
-       aL(k) = r(k-1)
+       !    aR(k) = r(k)
+       !    aL(k) = r(k-1)
        ! end if
        dL(k) = Half * (aR(k) - aL(k))
        dR(k) = Half * (aR(k) + aL(k)) - var_old(k)
@@ -762,7 +763,7 @@ contains
     end do
   end subroutine remap2W
 
-  subroutine remap4_tile (N, var_new, z_new, var_old, z_old)
+  subroutine remap4 (N, var_new, z_new, var_old, z_old)
     ! Parabolic WENO enhanced by quartic power-law reconciliation step. 
     ! Profile in each grid box is then given by a quartic polynomial.
     !
@@ -780,7 +781,7 @@ contains
     real(8), parameter      :: Zero=0.0_8, One=1.0_8, Two=2.0_8, Three=3.0_8, Four=4.0_8, Six=6.0_8
     real(8), dimension(1:N) :: Hz
     real(8), dimension(0:N) :: aL, aR, dL, dR, d, FC, r, r1
-    logical, parameter      :: NEUMANN = .false. ! Neumann option commented out for efficiency
+    logical, parameter      :: NEUMANN = .true. ! Neumann option commented in for efficiency
     !
     ! Parabolic WENO reconstruction: The second and third loops below
     !---------- ---- --------------- compute left and right side limits
@@ -796,7 +797,7 @@ contains
     do k = N-1, 1, -1
        FC(k) = One / (Hz(k+1) + Hz(k))
        d(k)  = FC(k) * (var_old(k+1) - var_old(k))
-    enddo
+    end do
 
     do k = 2, N-1
        deltaR = Hz(k)*d(k)
@@ -841,13 +842,13 @@ contains
        r1(k) = (deltaR*aR(k) + deltaL*aL(k+1)) / (deltaR + deltaL)
     end do      !--> discard aR,aL,dR,dL
 
-    if (NEUMANN) then
-       r1(N) = ThreeHalfth*var_old(N) - Half*r1(N-1)
-       r1(0) = ThreeHalfth*var_old(1) - Half*r1(1)
-    else
-       r1(N) = Two*var_old(N) - r1(N-1)
-       r1(0) = Two*var_old(1) - r1(1)
-    end if
+    !if (NEUMANN) then
+    r1(N) = ThreeHalfth*var_old(N) - Half*r1(N-1)
+    r1(0) = ThreeHalfth*var_old(1) - Half*r1(1)
+    ! else
+    !    r1(N) = Two*var_old(N) - r1(N-1)
+    !    r1(0) = Two*var_old(1) - r1(1)
+    ! end if
     !
     ! Power-law reconciliation: Starts with computation of side
     !------ --- --------------- limits dR,dL of the first derivative
@@ -912,7 +913,7 @@ contains
        else
           Hdd = Hz(k+1) * (dL(k+1) - d(k))
           rr = r1(k+1) - var_old(k+1)
-       endif
+       end if
        rr = abs (rr)
 
        Ampl = OneFifth * Hdd * rr
@@ -922,20 +923,20 @@ contains
           Ampl = Ampl * (rr + 0.0363636363636363636*Hdd) / cff
        else
           Ampl = Zero
-       endif
+       end if
        r(k) = r1(k) + Ampl
     end do
 
     ! if (NEUMANN) then
-    !    r(0) = ThreeHalfth*var_old(1) - Half*r(1)
-    !    r(N) = ThreeHalfth*var_old(N) - Half*r(N-1)
-    !    d(0) = Zero
-    !    d(N) = Zero
+    r(0) = ThreeHalfth*var_old(1) - Half*r(1)
+    r(N) = ThreeHalfth*var_old(N) - Half*r(N-1)
+    d(0) = Zero
+    d(N) = Zero
     ! else
-    r(0) = Two*var_old(1) - r(1)
-    r(N) = Two*var_old(N) - r(N-1)
-    d(0) = d(1)
-    d(N) = d(N-1)
+    !    r(0) = Two*var_old(1) - r(1)
+    !    r(N) = Two*var_old(N) - r(N-1)
+    !    d(0) = d(1)
+    !    d(N) = d(N-1)
     ! end if
     !
     ! Remapping step: This operation consists essentially of three
@@ -973,5 +974,5 @@ contains
     do k = 1, N
        var_new(k) = (Hz(k)*var_old(k) + FC(k)-FC(k-1)) / (z_new(k) - z_new(k-1))
     end do
-  end subroutine remap4_tile
+  end subroutine remap4
 end module remap_mod
