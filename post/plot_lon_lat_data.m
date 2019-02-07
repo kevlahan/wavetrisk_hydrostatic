a209a5d954af1c5499abc6febd6af8bd9424f63f
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2019-02-07 17:36:28 +0100

    Improve colorbar labels.

--- post/plot_lon_lat_data.m
+++ post/plot_lon_lat_data.m
@@ -24,7 +24,8 @@ end
 if not(lines)
     set(h,'LineColor','none')
 end
-colormap(jet(numel(c_scale)-1));caxis([min(c_scale) max(c_scale)]);c=colorbar;c.Label.String=v_title;c.Label.FontSize=16;
+colormap(jet(numel(c_scale)-1));caxis([min(c_scale) max(c_scale)]);c=colorbar;
+c.Label.String=v_title;c.Label.FontSize=16;c.YTick=c_scale;
 axis('equal'); 
 set(gca,'ytick',v_yticks,'yticklabels',{'90S','60S','30S','0','30N','60N','90N'});
 if (shift)
