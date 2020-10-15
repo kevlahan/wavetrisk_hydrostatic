acfba0f4809dbe946894e2b5b2c8314a07a4de86
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2020-10-15 17:09:42 -0400

    Remove matrix size limits on 2d projection matrices.

--- test/flat_projection_data/test_case_module.f90
+++ test/flat_projection_data/test_case_module.f90
@@ -193,12 +193,6 @@ contains
     read (fid,*) varname, press_save
     close(fid)
 
-    if (N > 2000) then
-       write (6,'(A,i5,A)') "N = ", N, " is too large. Maximum allowed value of N = 2000."
-       write (6,'(A)') "Modify export_2d in io.f90 if necessary."
-       stop
-    end if
-
     if (rank==0) then
        write (6,'(A,A)')      "test_case              = ", trim (test_case)
        write (6,'(A,A)')      "run_id                 = ", trim (run_id)
