547323493855159cdb129a904ffc3f90a8392b2d
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2021-01-14 09:55:03 -0500

    Move computation of minimum mass after remapping so remapped mass is displayed.

--- src/main.f90
+++ src/main.f90
@@ -231,8 +231,8 @@ contains
     end if
 
     ! If necessary, remap vertical coordinates
-    min_mass = cpt_min_mass ()
     if (remap .and. modulo (istep, iremap) == 0) call remap_vertical_coordinates
+    min_mass = cpt_min_mass ()
     
     ! Add diffusion
     if (modulo (istep_cumul, n_diffuse) == 0 .and. Laplace_order_init /= 0) then
