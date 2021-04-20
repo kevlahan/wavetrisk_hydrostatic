0af64843e3a63e0d951772192d5ecbc25a17fbbf
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2021-04-20 17:36:49 -0400

    Add new results files and model description.

---
+++ test/tke1d/temp_Kt.m
@@ -0,0 +1,33 @@
+%% TKE profile
+clear all
+time = 1;
+remote_file = ['~/hydro/tke/tke1d.6.' sprintf( '%04d', time)];
+local_file  = remote_file;
+scp_cmd     = ['scp if.mcmaster.ca:' remote_file ' ' local_file];
+unix (sprintf(scp_cmd));
+T = load(local_file);
+
+% Theoretical value of mixing depth
+u_s = 0.01; N_0 = 0.01; t = 30;
+Dm = - 1.05 * u_s * sqrt(t*60^2/N_0);
+
+%figure; 
+figure; sgtitle("\Delta t = 1200s N = 100 levels",'fontsize', 24);
+
+% Use Florian's dimensions
+
+subplot(1,2,1);plot(T(:,6),T(:,5),'b-','linewidth',2);hold on;
+plot(T(:,6),ones(size(T(:,6)))*Dm,'r--','linewidth',2);
+xlabel('Temperature [\circC]');ylabel('Depth [m]'); set(gca,'fontsize',16); grid on;
+axis([13.25 15.4 -52.4 2.4]);
+pbaspect([0.64 1 1]);
+
+subplot(1,2,2);plot(T(:,3),T(:,2),'b-','linewidth',2);
+xlabel('Kt [m^2/s]');ylabel('Depth [m]'); set(gca,'fontsize',16); grid on;
+axis([-1e-3 2.25e-2 -52.4 2.4]);
+pbaspect([0.64 1 1]);
+
+% subplot(1,3,3);plot(T(:,4),T(:,2),'b-','linewidth',2);
+% xlabel('Kv [m^2/s]');ylabel('Depth [m]'); set(gca,'fontsize',16); grid
+% axis([0 2e-2 -50 1.2]);
+% pbaspect([0.6 1 1]);
