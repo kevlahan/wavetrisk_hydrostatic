a43242710831c1edaf6bd4a10e5f4bf30f11560e
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2021-10-06 13:37:10 -0400

    Improve comments.

--- src/param_J6.f90
+++ src/param_J6.f90
@@ -1,5 +1,7 @@
 module param_mod
-  ! Maximum number of cores = 10 * 2^(2*DOMAIN_LEVEL)
-  integer, parameter :: DOMAIN_LEVEL = 2 ! <= 3 (MIN_LEVEL - PATCH_LEVEL - 1 with PATCH_LEVEL >= 2)
-  integer, parameter :: MIN_LEVEL    = 6 ! sets coarsest grid
+  ! Maximum number of cores <= number of domains = 10 * 2^(2*DOMAIN_LEVEL)
+  ! PATCH_LEVEL = MIN_LEVEL - (DOMAIN_LEVEL + 1) >= 2
+  ! Total nodes = 10 * 2^(2*MIN_LEVEL) 
+  integer, parameter :: DOMAIN_LEVEL = 2 ! determines maximum cores and PATCH_LEVEL
+  integer, parameter :: MIN_LEVEL    = 6 ! coarsest grid
 end module param_mod
