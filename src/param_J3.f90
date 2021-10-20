1a1829bc8712e538fde5151d3a7a69abb7499493
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2021-10-20 09:32:33 -0700

    New (coarsest possible) grid J3 option.

---
+++ src/param_J3.f90
@@ -0,0 +1,7 @@
+module param_mod
+  ! Maximum number of cores <= number of domains = 10 * 2^(2*DOMAIN_LEVEL)
+  ! PATCH_LEVEL = MIN_LEVEL - (DOMAIN_LEVEL + 1) >= 2
+  ! Total nodes = 10 * 2^(2*MIN_LEVEL) 
+  integer, parameter :: DOMAIN_LEVEL = 0 ! determines maximum cores and PATCH_LEVEL
+  integer, parameter :: MIN_LEVEL    = 3 ! coarsest grid
+end module param_mod
