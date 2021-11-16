2847e13fe567366be771f11aeaaf5d86066818c9
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2021-11-16 14:44:39 -0500

    Change order of ampi calls to avoid rare segmentation faults.

--- src/main.f90
+++ src/main.f90
@@ -373,8 +373,9 @@ contains
 #ifdef AMPI
     if (rank == 0) write (6,'(/,a)') "Rebalancing using AMPI ..."
     call MPI_Barrier (MPI_COMM_WORLD, ierror)
-    call AMPI_Load_reset_measure (ierror)
     call AMPI_Migrate (AMPI_INFO_LB_SYNC, ierror)
+
+    call AMPI_Load_reset_measure (ierror)
 #endif
   end subroutine restart
 
