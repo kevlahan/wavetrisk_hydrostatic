1e149b73ca0a1ecbf5245777dcfeb38d80a6f7e4
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2022-10-18 14:53:58 -0400

    Simplify spectrum post-processing.

--- post/spherical_harmonic_analysis.m
+++ post/spherical_harmonic_analysis.m
@@ -1,60 +1,79 @@
-%% Analyse spherical harmonic data
-test_case  = 'drake';
-type       = 'barotropic';
-local      = true;
-machine    = 'if.mcmaster.ca';
-%machine    = 'niagara.computecanada.ca';
+%% Load data
+clear
+test_case = 'drake';
+%machine  = 'if.mcmaster.ca';
+machine   = 'niagara.computecanada.ca';
 
-% Parameters
-scale_earth =  6;
+%run_id = '1layer_slow' ; type = 'barotropic_spec';       name_type = '1 layer'; cp_id = '0031';
+%run_id = '2layer_slow' ; type = 'total_2_spec';          name_type = '2 layer'; cp_id = '0031';
+%run_id = '6layer_test' ; type = 'barotropic_curlu_spec'; name_type = '6 layer'; cp_id = '0038';
+run_id = '12layer_test'; type = 'barotropic_curlu_spec'; name_type = '12 layer'; cp_id = '0008';
+
+local_file = load_data(test_case, run_id, cp_id, type, machine);
+
+%% Plot energy spectra
+p = -2.5; % compare to power law k^p
+
+% Physical parameters of simulation
+visc        =  0.493;
+uwbc        =  0.8; 
 scale_omega =  6;
-uwbc        =  1.0;
-
-theta       =  45; % latitude at which to calculate f0 and beta
+scale_earth =  6;
 omega       =  7.29211e-5/scale_omega;
-radius      =  6371.229e3/scale_earth;   
+radius      =  6371.229e3/scale_earth; 
 g           =  9.80616;
 drho        = -4;
 ref_density =  1028;
 H1          =  3e3;
 H2          =  1e3;
 H           =  H1 + H2;
+theta       =  45; % latitude at which to calculate f0 and beta
 f0          =  2*omega*sin(deg2rad(theta));
 beta        =  2*omega*cos(deg2rad(theta))/radius;
+%r_b         =  1.3e-8; % two-layer
+r_b         =  1e-7;
 c0          =  sqrt(g*H);
-c1          =  sqrt (g*abs(drho)/2/ref_density * H2*(H-H2)/H);
-visc        =  8.25e-1;
+%c1          =  sqrt (g*abs(drho)/ref_density * H2*(H-H2)/H); % two-layer
+c1          =  5.56; % m/s
 
 % Lengthscales (km)
-lambda0    = c0/f0;             % external radius of deformation
-lambda1    = c1/f0;             % internal radius of deformation
-deltaSM    = uwbc/f0;           % submesoscale
-deltaI     = sqrt(uwbc/beta);   % inertial layer
-deltaM     = (visc/beta)^(1/3); % Munk layer 
+lambda0    = c0/f0/1e3;             % external radius of deformation
+lambda1    = c1/f0/1e3;             % internal radius of deformation
+deltaS     = r_b / beta/1e3;        % Stommel layer
+deltaSM    = uwbc/f0/1e3;           % submesoscale
+deltaI     = sqrt(uwbc/beta)/1e3;   % inertial layer
+deltaM     = (visc/beta)^(1/3)/1e3; % Munk layer 
 
-file_base   = [run_id '_' cp_id '_' type];
-remote_file = ['~/hydro/' test_case '/' file_base];
-local_file  = ['~/hydro/' test_case '/' file_base];
-%% Load local region data
-if ~strcmp(machine,'mac') 
-    scp_cmd     = ['scp ' machine ':' remote_file ' ' local_file];
-    unix (sprintf(scp_cmd));
-end
+pspec = load(local_file);
+pspec(:,2) = pspec(:,2)./pspec(:,1).^2;                     % convert vorticity spectrum to energy spectrum integrated over shells
+scales = 2*pi*radius/1e3./sqrt(pspec(:,1).*(pspec(:,1)+1)); % equivalent length scale (Jeans relation)
+
+% Power spectrum
+loglog(scales(:,1),pspec(:,2),'b-','linewidth',3,'DisplayName',name_type);hold on;grid on;
+
+% Log-law comparison
+range = [deltaI deltaSM ]; % range to fit power law 
+powerlaw (scales, 1.5*pspec(:,2), range, p, 'r--')
+
+axis([4e0 1e4 1e-10 1e0]);
+set (gca,'fontsize',20);
+xlabel("\lambda (km)");ylabel("S(\lambda)");
+set (gca,'Xdir','reverse');legend;
+%plot_scale(lambda0,"\lambda_0");
+%plot_scale(lambda1,"\lambda_1");
+plot_scale(deltaSM,"\delta_{SM}");
+plot_scale(deltaI,"\delta_{I}");
+%plot_scale(deltaS,"\delta_{S}");
+%plot_scale(deltaM,"\delta_{M}");
 %% Plot local region
-local      = true;
-region     = 'mid';
-run_id     = "2layer_normal";
-cp_id      = "0031";
-type       = "barotropic";
-local_file = run_id + "_" + cp_id + "_" + type;
+local  = true;
+region = 'mid';
 
 fid = fopen(local_file);
 data = fread(fid,'double'); 
 N = round(-3/2 + sqrt(2*(numel(data)+1)));
-%data = reshape (data, N+1, N/2+1)';
 data = reshape (data, N+1, N/2+1)';
-dmin = min(min(data));
-dmax = max(max(data));
+dmin = min(min(data)); dmax = max(max(data));
 fprintf('Minimum value of data = %8.4e\n', dmin);
 fprintf('Maximum value of data = %8.4e\n', dmax);
 
@@ -112,75 +131,6 @@ lines   = 0;
 shift   = 0;
 plot_lon_lat_data(data(1:4:end,1:4:end), lon(1:4:end), lat(1:4:end), c_scale, v_title, smooth, shift, lines);
 axis(ax);
-%% Load spectrum data
-run_id      = '12layer_test'; type = 'barotropic_curlu'; cp_id = '0008';
-
-test_case   = 'drake';
-name_type   = 'Barotropic';
-%machine     = 'if.mcmaster.ca';
-machine    = 'niagara.computecanada.ca';
-
-file_base   = [run_id '_' cp_id '_' type '_spec'];
-remote_file = ['~/hydro/' test_case '/' file_base];
-local_file  = ['~/hydro/' test_case '/' file_base];
-scp_cmd     = ['scp ' machine ':' remote_file ' ' local_file];
-if ~strcmp(machine,'mac') 
-    unix (sprintf(scp_cmd));
-end
-
-% Physical parameters of simulation
-visc        =  0.493;
-uwbc        =  1; 
-scale_omega =  6;
-scale_earth =  6;
-theta       =  45; % latitude at which to calculate f0 and beta
-omega       =  7.29211e-5/scale_omega;
-radius      =  6371.229e3/scale_earth; 
-g           =  9.80616;
-drho        = -4;
-ref_density =  1028;
-H1          =  3e3;
-H2          =  1e3;
-H           =  H1 + H2;
-f0          =  2*omega*sin(deg2rad(theta));
-beta        =  2*omega*cos(deg2rad(theta))/radius;
-r_b         =  1e-7;
-c0          =  sqrt(g*H);
-%c1          =  sqrt (g*abs(drho)/ref_density * H2*(H-H2)/H); % two-layer
-c1          =  5.56; % m/s
-
-% Lengthscales (km)
-lambda0    = c0/f0/1e3;             % external radius of deformation
-lambda1    = c1/f0/1e3;             % internal radius of deformation
-deltaS     = r_b / beta/1e3;        % Stommel layer
-deltaSM    = uwbc/f0/1e3;           % submesoscale
-deltaI     = sqrt(uwbc/beta)/1e3;   % inertial layer
-deltaM     = (visc/beta)^(1/3)/1e3; % Munk layer 
-
-%% Plot energy spectrum
-p     = -2.5;     % compare to power law k^p
-range = [lambda1 deltaSM ]; % range to fit power law 
-
-pspec = load(local_file);
-pspec(:,2) = pspec(:,2)./pspec(:,1).^2;                     % convert vorticity spectrum to energy spectrum integrated over shells
-scales = 2*pi*radius/1e3./sqrt(pspec(:,1).*(pspec(:,1)+1)); % equivalent length scale (Jeans relation)
-
-% Power spectrum
-loglog(scales(:,1),pspec(:,2),'b-','linewidth',3,'DisplayName',name_type);hold on;grid on;
-
-% Log-law comparison
-powerlaw (scales, 1.5*pspec(:,2), range, p, 'r--')
-
-axis([4e0 1e4 1e-10 1e0]);
-set (gca,'fontsize',20);
-xlabel("\lambda (km)");ylabel("S(\lambda)");
-set (gca,'Xdir','reverse');legend;
-plot_scale(lambda0,"\lambda_0");
-plot_scale(lambda1,"\lambda_1");
-plot_scale(deltaSM,"\delta_{SM}");
-%plot_scale(deltaS,"\delta_{S}");
-%plot_scale(deltaM,"\delta_{M}");
-
 %%
 function powerlaw (scales, power, range, p, col)
 % Plots power law -p between scales s1 and s2 (s2 > s1) with color col
@@ -204,3 +154,15 @@ h=loglog([x x], y, 'k','linewidth',1.5);
 set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
 text(0.92*scale,10*y(1),name,"fontsize",16)
 end
+
+function [local_file] =  load_data (test_case, run_id, cp_id, type, machine)
+file_base   = [run_id '_' cp_id '_' type];
+remote_file = ['~/hydro/' test_case '/' file_base];
+local_file  = ['~/hydro/' test_case '/' file_base];
+scp_cmd     = ['scp ' machine ':' remote_file ' ' local_file];
+if ~strcmp(machine,'mac')
+    unix (sprintf(scp_cmd));
+end
+end
+
+
