76339d459e21a872ed5b883d100692d14cdec10c
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2022-12-13 13:38:09 -0500

    Correct buoyancy call.

--- src/remap.f90
+++ src/remap.f90
@@ -251,8 +251,7 @@ contains
 
     ! Old buoyancy
     do k = 1, zlevels
-       !theta_old(k) = buoyancy (dom, i, j, k, offs, dims, sol)
-       theta_old(k) = buoyancy (dom, i, j, k, offs, dims)
+       theta_old(k) = buoyancy (dom, i, j, k, offs, dims, sol)
     end do
 
     ! Remap density
