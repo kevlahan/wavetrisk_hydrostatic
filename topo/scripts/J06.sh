87e75caf6cf280011c8aece243ccb96db9036dba
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2023-11-27 14:58:06 -0500

    Add data identifier to NCAR topography scripts.

--- topo/scripts/J06.sh
+++ topo/scripts/J06.sh
@@ -3,6 +3,7 @@
 --no_ridges \
 --smoothing_over_ocean \
 --grid_descriptor_file='J06_topo_grid.nc' \
+--source_data_identifier='gmted2010_modis_bedmachine-ncube0540-220518.nc' \
 --intermediate_cs_name='gmted2010_bedmachine-ncube0540-220518.nc' \
 --output_grid='J06' \
 --smoothing_scale=120.0 \
