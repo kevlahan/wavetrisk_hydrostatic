2a40657de2b3bbf042efb04cc99184e46e6090b1
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2023-12-01 10:22:59 -0500

    Always compute wavelets, adapt grid and compute inverse wavelet transform. Ensures that wavelets exist for checkpointing (needed even on single scale non-adaptive grid) or other purposes.

--- src/main.f90
+++ src/main.f90
@@ -256,16 +256,14 @@ contains
     else
        Laplace_order = 0
     end if
-
+    
     ! Adapt grid
-    if (min_level /= max_level .and. modulo (istep, iadapt) == 0) then
-       if (vert_diffuse .or. (remap .and. modulo (istep, iremap) == 0)) &
-            call WT_after_step (sol(:,1:zlevels), wav_coeff(:,1:zlevels), level_start-1)
-       if (zmin < 1) &
-            call WT_after_step (sol(:,zmin:0), wav_coeff(:,zmin:0), level_start-1)
-       call adapt (set_thresholds)
-       call inverse_wavelet_transform (wav_coeff, sol, jmin_in=level_start)
-    end if
+    if (vert_diffuse .or. (remap .and. modulo (istep, iremap) == 0)) &
+         call WT_after_step (sol(:,1:zlevels), wav_coeff(:,1:zlevels), level_start-1)
+    if (zmin < 1) &
+         call WT_after_step (sol(:,zmin:0), wav_coeff(:,zmin:0), level_start-1)
+    call adapt (set_thresholds)
+    call inverse_wavelet_transform (wav_coeff, sol, jmin_in=level_start)
 
     call update
     
