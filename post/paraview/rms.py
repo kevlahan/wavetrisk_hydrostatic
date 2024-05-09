675d4bc5d3daff0d508a2c67f49f0b1151d8428c
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2024-05-09 15:09:51 -0400

    Update rms.py.

--- post/paraview/rms.py
+++ post/paraview/rms.py
@@ -3,25 +3,28 @@
 import sys
 import numpy as np
 import vtk
-
-
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 # Define rms function
 def rms(arr):
     return np.sqrt(np.mean(np.square(arr)))
 
 # Main program
-if (len(sys.argv)<4) :
+if (len(sys.argv)<5) :
     print("Usage: python energy.py base_vtk_file tstart tend field")
+    print("base_vtk_file = prefix of vtk files to load (e.g. HS_J7_001)")
     print("tstart = first file")
     print("tend   = last last file")
+    print("dt     = save interval (days)")
     print("field  = field to analyze (mass, vorticity, etc)")
+    print("Output saved to base_vtk_file.txt")
     exit(0)
 
 file_base = sys.argv[1]
 j1        = int(sys.argv[2])
 j2        = int(sys.argv[3])
-field     = sys.argv[4]
+dt        = int(sys.argv[4])
+field     = sys.argv[5]
 
 outfile = file_base+"_rms.txt"
 f = open(outfile, "w")
@@ -34,37 +37,37 @@ for j in range (j1, j2+1):
     vtk_data.ReadAllScalarsOn()
     vtk_data.SetFileName(infile)
     vtk_data.Update()
-
+    
     cell_data = vtk_data.GetOutput().GetCellData()
+    data = cell_data.GetArray(field)
 
-    data      = cell_data.GetArray(field)
+    rms_data = rms(data)
 
-    calc = vtk.vtkArrayCalculator()
-    calc.SetInputData(vtk_data.GetOutput())
-    calc.SetFunction("5")
-    calc.SetResultArrayName("MyResults")
-    calc.Update()
+    print("{:>2d} {:e}".format(dt*j, rms_data))
+    f.write("{:>2d} {:e}\n".format(dt*j, rms_data))
 
-    num_cells = vtk_data.GetOutput().GetNumberOfCells()
-    print(vtk)
+    # num_cells = vtk_data.GetOutput().GetNumberOfCells()
+    
+    # calc = vtk.vtkArrayCalculator()
+    # calc.SetInputData(vtk_data.GetOutput())
+    # calc.SetFunction("mass**2")
+    # calc.SetResultArrayName("square")
+    # calc.Update()
 
-    #calculator = vtk.vtkCalculator()
+    # integrate = vtk.vtkIntegrateAttributes()
+    # integrate.SetInputData(vtk_data.GetOutput())
+    # integrate.SetDivideAllCellDataByVolume(1)
+    # integrate.Update()
 
-    integrate = vtk.vtkIntegrateAttributes()
+    # integrated = integrate.GetOutput()
+    # cd = integrated.GetCellData()
 
-    integrate.SetInputData(vtk_data.GetOutput())
-    integrate.SetDivideAllCellDataByVolume(1)
-    integrate.Update()
+    # print("Integrated cell data")
+    # for i in range(cd.GetNumberOfArrays()) :
+    #     arrayName = cd.GetArray(i).GetName()
+    #     arrayValue = cd.GetArray(i).GetTuple(0)[0]
+    #     print("%s: %f" % (arrayName, arrayValue))
 
-    integrated = integrate.GetOutput()
-    cd = integrated.GetCellData()
-
-    print("Integrated cell data")
-    for i in range(cd.GetNumberOfArrays()) :
-        arrayName = cd.GetArray(i).GetName()
-        arrayValue = cd.GetArray(i).GetTuple(0)[0]
-        print("%s: %f" % (arrayName, arrayValue))
 
     
-    f.write("{:d} {:e}\n".format(j, rms(data)))
 
