42048a5f1a045ac26f92827d07c96eaf9b04c78e
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2024-10-04 09:55:17 -0400

    Cleanup and simplification of radiative transfer model. Remove data logging and openmp directives.

--- src/physics/simple_physics/phyparam/physics/radiative_mod.F90
+++ src/physics/simple_physics/phyparam/physics/radiative_mod.F90
@@ -1,14 +1,13 @@
 module radiative_mod
-#include "use_logging.h"
   use comgeomfi
-  use callkeys, only : diurnal, lverbose
+  use callkeys, only : diurnal
   implicit none
   private
   save
-  real, parameter :: pi = 2.0 * asin(1.0), solarcst = 1370.0, stephan = 5.67e-08, height_scale = 10000.0, ps_rad = 1.e5
+  real, parameter :: pi = 2.0 * asin (1.0), Solarcst = 1370.0, Stephan = 5.67e-08, height_scale = 10000.0, Ps_rad = 1e5
   public          :: radiative_tendencies
 contains
-  subroutine radiative_tendencies (lwrite, ngrid, igout, nlayer, gmTime, zdTime, zDay, pPlev, pPlay, pT, pdT, FluxRad)
+  subroutine radiative_tendencies (ngrid, nlayer, gmTime, zdTime, zDay, pPlev, pPlay, pT, FluxRad)
     USE planet
     use phys_const,     only : planet_rad, unjours
     use astronomy,      only : orbite, solarlong
@@ -19,21 +18,21 @@ contains
     use writefield_mod, only : writefield
 
     ! Input variables
-    integer,                         intent(in)    :: ngrid, igout, nlayer
-    real,                            intent(in)    :: gmTime            ! fraction of the day
-    real,                            intent(in)    :: zdTime            ! time step [s]
-    real,                            intent(in)    :: zDay              ! elapsed days (and fraction thereof)
+    integer,                         intent(in)    :: ngrid   ! number of columns
+    integer,                         intent(in)    :: nlayer  ! number of layers
+    real,                            intent(in)    :: gmTime  ! fraction of the day
+    real,                            intent(in)    :: zdTime  ! time step [s]
+    real,                            intent(in)    :: zDay    ! elapsed days (and fraction thereof)
     real, dimension(ngrid,nlayer),   intent(in)    :: pPlay
     real, dimension(ngrid,nlayer+1), intent(in)    :: pPlev
-    real, dimension(ngrid,nlayer),   intent(in)    :: pT                ! temperature [K]
-    logical,                         intent(in)    :: lwrite            ! true for output
 
-    real, dimension(ngrid,nlayer),   intent(inout) :: pdT               ! tendency of temperature [K/s]
-    real, dimension(ngrid),          intent(out)   :: fluxrad           ! net surface flux
+    ! Output
+    real, dimension(ngrid,nlayer),   intent(inout) :: pT       ! temperature [K] (advanced from t -> t+dt)
+    real, dimension(ngrid),          intent(out)   :: FluxRad  ! net surface flux
 
     ! Local variables
     integer                       :: ig, l
-    real                          :: zls, zInsol, ztim1,ztim2,ztim3, dist_sol, declin
+    real                          :: zls, zInsol, ztim1, ztim2, ztim3, dist_sol, declin
     real, dimension(ngrid)        :: fract     ! day fraction
     real, dimension(ngrid)        :: zFluxSW   ! short-wave flux at surface
     real, dimension(ngrid)        :: zFluxlw   ! short-wave flux at surface
@@ -42,70 +41,32 @@ contains
     real, dimension(ngrid,nlayer) :: zdTsw     ! short-wave temperature tendency
     real, dimension(ngrid,nlayer) :: zdTlw     ! long-wave temperature tendency
 
-    ! ----------------------------------------------------
-    !   2.1 Insolation
-    ! ----------------------------------------------------
+    !  Insolation
     call solarlong (zday, zls)
     call orbite (zls, dist_sol, declin)
 
-    if (diurnal) THEN
-       ztim1 =   SIN (declin)
-       ztim2 =   COS (declin) * COS (2.0 * pi * (zday - 0.5))
-       ztim3 = - COS (declin) * SIN (2.0 * pi * (zday - 0.5))
+    if (diurnal) then
+       ztim1 =   sin (declin)
+       ztim2 =   cos (declin) * cos (2.0 * pi * (zday - 0.5))
+       ztim3 = - cos (declin) * sin (2.0 * pi * (zday - 0.5))
 
        call solang (ngrid, sinlon, coslon, sinlat, coslat,  ztim1, ztim2, ztim3, mu0, fract)
-       if (lverbose) THEN
-          WRITELOG (*,*) 'day, declin, sinlon,coslon, sinlat, coslat'
-          WRITELOG (*,*) zday, declin, sinlon(igout), coslon(igout), sinlat(igout), coslat(igout)
-          LOG_DBG ('radiative_tendencies')
-       end if
     else
-       WRITELOG (*,*) 'declin,ngrid,planet_rad', declin, ngrid, planet_rad
-       LOG_DBG ('radiative_tendencies')
-       call mucorr (ngrid, declin, lati, mu0,fract, height_scale, planet_rad)
+       call mucorr (ngrid, declin, lati, mu0, fract, height_scale, planet_rad)
     end if
 
-    zinsol = solarcst / dist_sol**2
+    zInsol = solarcst / dist_sol**2
 
-    ! ----------------------------------------------------
-    !    2.2 Radiative tendencies and fluxes:
-    ! ----------------------------------------------------
-    call sw (ngrid, nlayer, diurnal, CoefVis, Albedo, pPlev, pS_Rad, mu0, fract, zInsol, zFluxSW, zdTsw, lverbose, lwrite)
-    call lw (ngrid, nlayer, coefir, Emissiv, pPlev, pS_rad, Tsurf, pT, zFluxlw, zdTlw, lverbose, lwrite)
+    ! Radiative tendencies and fluxes:
+    call sw (ngrid, nlayer, diurnal, CoefVis, Albedo, pPlev, Ps_rad, mu0, fract, zInsol, zFluxSW, zdTsw)
+    call lw (ngrid, nlayer, coefir, Emissiv, pPlev, Ps_rad, Tsurf, pT, zFluxlw, zdTlw)
 
-
-    ! ----------------------------------------------------
-    !    2.4 Surface fluxes
-    ! ----------------------------------------------------
-    Fluxrad = Emissiv * zfluxlw + zFluxsw * (1.0 - Albedo)
+    ! 2.4 Surface fluxes
+    FluxRad = Emissiv * zFluxlw + zFluxsw * (1.0 - Albedo)
     zPlanck = Emissiv * Stephan * Tsurf**4
     FluxRad = Fluxrad - zPlanck
 
-
-    ! ----------------------------------------------------
-    !    2.5 Temperature tendencies
-    ! ----------------------------------------------------
-    pdT = pdT + zdTsw + zdTlw
-
-
-    if (lverbose) THEN
-       WRITELOG (*,*) 'Diagnostics for radiation'
-       WRITELOG (*,*) 'albedo, emissiv, mu0,fract,Frad,Planck'
-       WRITELOG (*,*) Albedo(igout), Emissiv(igout), mu0(igout), fract(igout), FluxRad(igout), zPlanck(igout)
-       WRITELOG (*,*) 'Tlay Play Plev dT/dt SW dT/dt lw (K/day)'
-       WRITELOG (*,*) 'unjours',unjours
-       DO l = 1,nlayer
-          WRITELOG (*,'(3f15.5,2e15.2)') pT(igout,l), pPlay(igout,l), pPlev(igout,l), zdTsw(igout,l), zdTlw(igout,l)
-       endDO
-       LOG_DBG ('radiative_tendencies')
-    end if
-
-    if (lwrite) THEN
-       call writefield ('mu0',   'Cosine zenithal angle', '',     mu0)
-       call writefield ('swsurf','SW surf',               'W/m2', zFluxsw)
-       call writefield ('lwsurf','LW surf',               'W/m2', zFluxlw)
-       call writefield ('dtsw',  'dtsw',                  ' ',    zdTsw)
-       call writefield ('dtlw',  'dtlw',                  ' ',    zdTsw)
-    end if
+    ! Temperature at t+dt
+    pT = pT + (zdTsw + zdTlw) * zdTime
   end subroutine radiative_tendencies
 end MODULE radiative_mod
--- src/physics/simple_physics/phyparam/physics/radiative_sw.F90
+++ src/physics/simple_physics/phyparam/physics/radiative_sw.F90
@@ -1,11 +1,10 @@
 module radiative_sw
-#include "use_logging.h"
   implicit none
   save
   private
   public :: sw
 contains
-  subroutine sw (ngrid, nlayer,ldiurn, CoefVis, Albedo, pLevel, pS_rad,pMu, pfract, pSolarf0, FsrFvis, dTsw, lverbose, lwrite)
+  subroutine sw (ngrid, nlayer,ldiurn, CoefVis, Albedo, pLevel, pS_rad,pMu, pfract, pSolarf0, FsrFvis, dTsw)
     !=======================================================================
     !
     !   Rayonnement solaire en atmosphere non diffusante avec un
@@ -14,21 +13,22 @@ contains
     !=======================================================================
     USE phys_const,     only : Cpp, g
     USE writefield_mod, only : writefield
-    integer,                         intent(in) :: ngrid            ! number of columns
-    integer,                         intent(in) :: nlayer           ! number of vertical layers
-    real,                            intent(in) :: pSolarf0         ! solar constant
-    real,                            intent(in) :: pS_rad, CoefVis  ! CoefVis = attenuation at p = pS_rad
-    real, dimension(ngrid),          intent(in) :: Albedo           ! Albedo
-    real, dimension(ngrid),          intent(in) :: pMu              ! cosine zenithal angle
-    real, dimension(ngrid),          intent(in) :: pFract           ! day fraction
-    real, dimension(ngrid,nlayer+1), intent(in) :: pLevel           ! pressure at interfaces
-    logical,                         intent(in) :: ldiurn, lverbose, lwrite
+    integer,                         intent(in) :: ngrid    ! number of columns
+    integer,                         intent(in) :: nlayer   ! number of vertical layers
+    real,                            intent(in) :: pSolarf0 ! solar constant
+    real,                            intent(in) :: CoefVis  ! CoefVis = attenuation at p = Ps_rad
+    real,                            intent(in) :: Ps_rad   !
+    real, dimension(ngrid),          intent(in) :: Albedo   ! Albedo
+    real, dimension(ngrid),          intent(in) :: pMu      ! cosine zenithal angle
+    real, dimension(ngrid),          intent(in) :: pFract   ! day fraction
+    real, dimension(ngrid,nlayer+1), intent(in) :: pLevel   ! interface pressures
+    logical,                         intent(in) :: ldiurn   ! diurnal cycle
 
     ! Output
-    real, dimension(ngrid),          intent(out) :: FsrFvis         ! net surface flux
-    real, dimension(ngrid,nlayer),   intent(out) :: dTsw            ! temperature tendency
+    real, dimension(ngrid),          intent(out) :: FsrFvis ! net surface flux
+    real, dimension(ngrid,nlayer),   intent(out) :: dTsw    ! temperature tendency
 
-    ! Fluxes are non-zero only on those points where the sun shines (Mu0 > 0)
+    ! Fluxes are non-zero only on those points where sun shines (Mu0 > 0)
     ! we compute only on those ncount points
     integer                            :: ncount
     integer, dimension(ngrid)          :: index
@@ -50,10 +50,10 @@ contains
 
     if (ldiurn) then
        ncount = 0
-       index = 0
+       index  = 0
        do ig = 1, ngrid
           if (pfract(ig) > 1e-6) then
-             ncount = ncount+1
+             ncount = ncount + 1
              index (ncount) = ig
           end if
        end do
@@ -74,17 +74,13 @@ contains
     zdTsw     = 0.0
     zU        = 0.0
 
-    !-----------------------------------------------------------------------
-    !   calcul des profondeurs optiques integres depuis p=0:
-    !----------------------------------------------------
+    ! Profondeurs optiques integres depuis p=0:
 
-    !-----------------------------------------------------------------------
-    !   calcul de la transmission depuis le sommet de l atmosphere:
-    !   -----------------------------------------------------------
+    ! Transmission depuis le sommet de l atmosphere:
     Flux_in(1:ncount) = pSolarf0 * zfract(1:ncount) * zMu(1:ncount)
 
-    ! calcul de la partie homogene de l opacite
-    tau0 = -0.5 * log (CoefVis) / pS_rad
+    ! Partie homogene de l opacite
+    tau0 = -0.5 * log (CoefVis) / Ps_rad
 
     zU(1:ncount,:) = tau0 * zPlev(1:ncount,:)
 
@@ -92,87 +88,24 @@ contains
        Flux_down(1:ncount,l) = Flux_in(1:ncount) * exp (- zU(1:ncount,l) / abs (zMu(1:ncount) + 1e-13))
     end do
 
-    !-----------------------------------------------------------------------
-    !   4. calcul du flux solaire arrivant sur le sol:
-    !   ----------------------------------------------
+    ! Flux solaire arrivant sur le sol:
     zFlux(1:ncount)     = (1.0 - zAlb(1:ncount)) * Flux_down(1:ncount,1) ! absorbed (net)
     Flux_up(1:ncount,1) =        zAlb(1:ncount)  * Flux_down(1:ncount,1) ! reflected (up)
 
-    !-----------------------------------------------------------------------
-    !   5.calcul des transmissions depuis le sol, cas diffus:
-    !   ------------------------------------------------------
+    ! Transmissions depuis le sol, cas diffus:
     do l = 2, nlayer+1
        flux_up(1:ncount,l) = Flux_up(1:ncount,1) * exp (- (zU(1:ncount,1) - zU(1:ncount,l)) * 1.66)
     end do
 
-    !-----------------------------------------------------------------------
-    !   3. taux de chauffage, ray. solaire direct:
-    !   ------------------------------------------
+    ! Taux de chauffage, ray. solaire direct:
     ! m.cp.dt = dflux/dz
     ! m = -(dp/dz)/g
-    zdTsw(1:ncount,1:nlayer) = (g/cpp) * (Flux_down(1:ncount,2:nlayer+1) - Flux_down(1:ncount,1:nlayer))  / (zplev(1:ncount,1:nlayer) - zplev(1:ncount,2:nlayer+1))
+    zdTsw(1:ncount,1:nlayer) = (g/cpp) * (Flux_down(1:ncount,2:nlayer+1) - Flux_down(1:ncount,1:nlayer))  &
+         / (zplev(1:ncount,1:nlayer) - zplev(1:ncount,2:nlayer+1))
 
-    !-----------------------------------------------------------------------
-    !   6. ajout a l echauffement de la contribution du ray. sol. reflechit:
-    !   -------------------------------------------------------------------
-    zdTsw(1:ncount,1:nlayer) = zdTsw(1:ncount,1:nlayer) + (g/Cpp) * (Flux_up(1:ncount,1:nlayer) - Flux_up(1:ncount,2:nlayer+1)) /( zPlev(1:ncount,1:nlayer) - zPlev(1:ncount,2:nlayer+1))
-
-
-    !------------------------
-    !   Diagnostics
-    !------------------------
-    if (lverbose) then
-       igout = ncount/2 + 1
-
-       WRITELOG (*,*) 'Diagnostique des transmission dans le spectre solaire'
-       WRITELOG (*,*) 'zfract, zmu, zalb, flux_in'
-       WRITELOG (*,*) ZFRACT(igout), ZMU(igout), ZALB(igout), flux_in(igout)
-
-       if (flux_in(igout) > 0.0) then
-          WRITELOG (*,*) 'Pression, quantite d abs, transmission'
-          do l = 1, nlayer+1
-             WRITELOG (*,*) zplev(igout,l),zu(igout,l),flux_down(igout,l)/flux_in(igout)
-          end do
-       end if
-       LOG_INFO ('rad_sw')
-
-       WRITELOG (*,*) 'Diagnostique des taux de chauffage solaires:'
-       WRITELOG (*,*) ' 2 flux solaire net incident sur le sol'
-       WRITELOG (*,*) zflux(igout)
-       LOG_INFO ('rad_sw')
-
-       if (flux_up(igout,1) > 0.0) then
-          WRITELOG (*,*) 'Diagnostique des taux de chauffage solaires'
-          WRITELOG (*,*) ' 3 transmission avec les sol'
-          WRITELOG (*,*) 'niveau     transmission'
-          do l=  1, nlayer+1
-             WRITELOG (*,*) l, flux_up(igout,l) / flux_up(igout,1)
-          end do
-          LOG_INFO ('rad_sw')
-       end if
-
-       WRITELOG (*,*) 'Diagnostique des taux de chauffage solaires:'
-       WRITELOG (*,*) ' 3 taux de chauffage total'
-       do l = 1, nlayer
-          WRITELOG(*,*) ZDTSW(igout,l)
-       end do
-       LOG_INFO( 'rad_sw')
-    end if
-
-    if (lwrite) then
-       call monscatter (ngrid, ncount, index, flux_in, buf1)
-       call writefield ('swtop','sw down toa','w/m2',buf1)
-
-       do l = 1, nlayer+1
-          call monscatter (ngrid, ncount, index, flux_down(:,l), buf2(:,l))
-       end do
-       call writefield ('swflux_down','downward sw flux','w/m2', buf2)
-
-       do l = 1, nlayer+1
-          call monscatter (ngrid, ncount, index, flux_up(:,l), buf2(:,l))
-       end do
-       call writefield ('swflux_up','upward sw flux','w/m2', buf2)
-    end if
+    ! Ajout l echauffement de la contribution du ray. sol. reflechit:
+    zdTsw(1:ncount,1:nlayer) = zdTsw(1:ncount,1:nlayer) + (g/Cpp) * &
+         (Flux_up(1:ncount,1:nlayer) - Flux_up(1:ncount,2:nlayer+1)) / ( zPlev(1:ncount,1:nlayer) - zPlev(1:ncount,2:nlayer+1))
 
     call monscatter (ngrid, ncount, index, zflux,fsrfvis)
     do l = 1, nlayer
@@ -181,10 +114,12 @@ contains
   end subroutine sw
 
   pure subroutine mongather (ngrid, n,index, a,b)
+    ! Input:
     integer,                   intent(in)  :: ngrid, n
     integer, dimension(n),     intent(in)  :: index
     real,    dimension(ngrid), intent(in)  :: a
 
+    ! Output:
     real,    dimension(n),     intent(out) :: b(n)
 
     integer :: i
@@ -199,10 +134,12 @@ contains
   end subroutine mongather
 
   pure subroutine monscatter (ngrid, n, index, b, a)
+    ! Input
     integer,                   intent(in)  :: ngrid, n
     integer, dimension(n),     intent(in)  :: index
     real, dimension(n),        intent(in)  :: b
 
+    ! Output
     real, dimension(ngrid),    intent(out) :: a
 
     integer :: i
