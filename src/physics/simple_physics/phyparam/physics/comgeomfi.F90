ed55339323978f851775746c96aab41bad01b038
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2024-10-09 10:05:10 -0400

    Simplify initialization for physics (soil).

--- src/physics/simple_physics/phyparam/physics/comgeomfi.F90
+++ src/physics/simple_physics/phyparam/physics/comgeomfi.F90
@@ -1,47 +1,25 @@
-MODULE comgeomfi
-  IMPLICIT NONE
-  SAVE
+module comgeomfi
+  implicit none
+  save
+  integer                         :: ngridmax, nlayermx, nsoilmx
+  real, dimension(:), allocatable :: long, lati, sinlon, coslon, sinlat, coslat
+contains
+  subroutine init_comgeomfi (klon, klev, longitude, latitude)
+    integer,               intent(in) :: klon, klev
+    real, dimension(klon), intent(in) :: longitude, latitude ! in radians
 
-  REAL, ALLOCATABLE :: long(:), lati(:), sinlon(:), coslon(:), sinlat(:), coslat(:)
-  INTEGER :: ngridmax, nlayermx, nsoilmx
-  !$OMP THREADPRIVATE(long,lati,sinlon,coslon,sinlat,coslat,totarea)
-  !$OMP THREADPRIVATE(ngridmax,nlayermx,nsoilmx)
-  !$acc declare create(sinlon, coslon, sinlat, coslat)
+    ngridmax = klon
+    nlayermx = klev
 
-CONTAINS
+    allocate (long(klon), lati(klon))
+    allocate (sinlon(klon), coslon(klon), sinlat(klon), coslat(klon))
 
-  SUBROUTINE init_comgeomfi(klon, klev, longitude, latitude, soil_levs)
-    INTEGER, INTENT(IN) :: klon, klev
-    REAL, INTENT(IN) :: longitude(klon), latitude(klon) ! in radians
-    INTEGER, INTENT(IN), OPTIONAL :: soil_levs ! number of soil levels
+    long  =  longitude
+    lati  =  latitude
 
-    ! Check if soil_levs is present and set number soil layers nsoilmx (if not set default)
-    if (present(soil_levs)) then
-       if (soil_levs == 0 .or. soil_levs == 1) then
-          ! Make sure soil_levs is not 0 or 1 (soil model not set up for this)
-          PRINT*, 'STOP!! the soil model can not be set to have ', soil_levs, ' layer(s)!'
-          STOP
-       end if
-       nsoilmx = soil_levs
-    else
-       nsoilmx = 10
-    end if
-
-    ngridmax=klon
-    nlayermx=klev
-    allocate(long(klon))
-    allocate(lati(klon))
-    allocate(sinlon(klon))
-    allocate(coslon(klon))
-    allocate(sinlat(klon))
-    allocate(coslat(klon))
-    long(:) = longitude(:)
-    lati(:) = latitude(:)
-    sinlat(:)=sin(lati(:))
-    coslat(:)=cos(lati(:))
-    sinlon(:)=sin(long(:))
-    coslon(:)=cos(long(:))
-    !$acc update device(sinlon, coslon, sinlat, coslat)
-  END SUBROUTINE init_comgeomfi
-
-END MODULE comgeomfi
+    sinlat = sin (lati)
+    coslat = cos (lati)
+    sinlon = sin (long)
+    coslon = cos (long)
+  end subroutine init_comgeomfi
+end module comgeomfi
