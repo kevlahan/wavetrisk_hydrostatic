148b6366f705f2908118c0025f97ed68c3591227
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2024-11-12 14:59:05 -0500

    Update rms post-processing of vtk files.

--- post/paraview/rms.py
+++ post/paraview/rms.py
@@ -6,38 +6,46 @@ from utilities import *
 
 # Input
 if (len(sys.argv)<5) :
-    print("Usage: python rms.py base_vtk_file tstart tend dt field")
-    print("base_vtk_file = prefix of vtk files to load (e.g. HS_J7_001)")
-    print("tstart = first file")
-    print("tend   = last last file")
-    print("dt     = save interval (days)")
-    print("field  = field to analyze (mass, vorticity, etc)")
-    print("Output saved to base_vtk_file.txt")
+    print("\nUsage: python rms.py base_vtk_file k1 k2 t1 t2 field\n")
+    print("Example: python3 rms.py drakeJ8Z60_hex 54 54 120 120 5 density\n")
+    print("base_vtk_file = prefix of vtk files to load (e.g. drakeJ8Z60_hex)")
+    print("k1            = first vertical layer")
+    print("k2            = last  vertical layer")
+    print("t1            = first time count")
+    print("t2            = last  time count")
+    print("dt            = save interval (time = dt*count days)")
+    print("field         = field to analyze (vorticity, density, OMEGA, etc\n")
+    print("Output is saved to base_vtk_file.txt")
     exit(0)
 
 file_base = sys.argv[1]
-j1        = int(sys.argv[2])
-j2        = int(sys.argv[3])
-dt        = int(sys.argv[4])
-field     = sys.argv[5]
+k1        = int(sys.argv[2])
+k2        = int(sys.argv[3])
+t1        = int(sys.argv[4])
+t2        = int(sys.argv[5])
+dt        = int(sys.argv[6])
+field     = sys.argv[7]
 
 outfile = file_base+"_rms.txt"
 f = open(outfile, "w")
-  
-for j in range (j1, j2+1):
-    infile = file_base+".1"+str(j).zfill(4)+".vtk"
 
-    # Load the input vtk file
-    vtk_data = vtk.vtkUnstructuredGridReader()
-    vtk_data.ReadAllScalarsOn()
-    vtk_data.SetFileName(infile)
-    vtk_data.Update()
+print()
+for t in range (t1, t2+1):
+    for k in range (k1, k2+1):
+        infile = file_base+"_"+str(k).zfill(3)+"_"+str(t).zfill(4)+".vtk"
+        print("rms %s of %s is" % (field, infile), end=" ")
 
-    # Compute rms
-    rms = rms_int (vtk_data.GetOutput(), "mass")
+        # Load the input vtk file
+        vtk_data = vtk.vtkUnstructuredGridReader()
+        vtk_data.ReadAllScalarsOn()
+        vtk_data.SetFileName(infile)
+        vtk_data.Update()
+
+        # Compute rms
+        rms = rms_int (vtk_data.GetOutput(), "density")
     
-    print("{:e}".format(rms))
-    f.write("{:>4d} {:e}\n".format(dt*j, rms))
+        print("%7.4f" % (rms)) 
+        f.write("%3d  %7.4f" % (k, rms)) 
 
     
 
