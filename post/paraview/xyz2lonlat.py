05bc4ce522fb2404267a753208d2186a702eca6f
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2025-10-02 09:27:56 -0400

    Update post processing to avoid gaps.

--- post/paraview/xyz2lonlat.py
+++ post/paraview/xyz2lonlat.py
@@ -193,7 +193,7 @@ for t in range (t1, t2+1):
                     if (np.abs(np.abs(coord[0])-180)<2*dtheta):  # modify to avoid gaps if necessary
                         coord[0] = np.sign(coord[0]) * 180
                         points.SetPoint(pid, coord)
-                    if (np.abs(np.abs(coord[1])-90)<dtheta/2): # modify to avoid gaps if necessary
+                    if (np.abs(np.abs(coord[1])-90)<dtheta): # modify to avoid gaps if necessary
                         coord[1] = np.sign(coord[1]) * 90
                         points.SetPoint(pid, coord)
             points.Modified()
