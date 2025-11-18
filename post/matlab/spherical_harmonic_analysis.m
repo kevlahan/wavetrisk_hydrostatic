109bdc6eae542f4d1c0a703fdc716dc9819a74b5
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2025-11-18 13:17:34 -0500

    Update spherical harmonics matlab analysis.

--- post/matlab/spherical_harmonic_analysis.m
+++ post/matlab/spherical_harmonic_analysis.m
@@ -12,16 +12,24 @@ if ~strcmp(machine,"mac")
 end
 
 %% Analyze spectrum data
-clear; clc; global KM; format short e
+clear; clc; global KM; global tanh_strat; format short e
 
-test_case = "drake";     % drake, jet, Simple
-level     = 8;           % resolution level
-zlevels   = 12;          % number of vertical layers
-layers    = 1:zlevels;   % vertical layers to analyze
-type      = "u";         % u, curlu or divu
-avg       = true;        % analyze average spectrum or individual spectra
-power     = true;        % plot power law fit
-plot_spec = true;        % plot spectrum
+dir        = "~/hydro/drake/J8Z60_tanh/";
+tanh_strat = true;        % tanh profile (or linear)
+test_case  = "drake";     % drake, jet, Simple
+level      = 8;           % resolution level
+zlevels    = 60;          % number of vertical layers
+type       = "u";         % u, curlu or divu
+avg        = true;        % analyze average spectrum or individual spectra
+power      = true;       % plot power law fit
+plot_spec  = true;        % plot spectrum
+
+if tanh_strat
+    layers = [1 23 54 60]; % tanh
+else
+    layers = [1 14 46 60]; % linear
+end
+%layers     = 1:zlevels;   % vertical layers to analyze
 
 if avg
     cp_min = 1; cp_max=cp_min; 
@@ -30,34 +38,56 @@ else
 end
 
 % Set physical parameters
-[H, lambda0, lambda1, deltaS, deltaSM, deltaI, deltaM, radius] = params(test_case); 
-range       = [deltaI deltaSM] * KM; % range for power law fit   for tanh
+[H, lambda0, lambda1, deltaS, deltaSM, deltaRh, deltaM, radius, Z] = params(test_case);
 
+% Power law fitting
+range = zeros(zlevels,2);
+if tanh_strat
+    range( 1:15,1) = 250; range(1 :15,2) = 80;
+    range(16:20,1) = 250; range(16:20,2) = 80;
+    range(21:35,1) = 260; range(21:35,2) = 110;
+    range(25:40,1) = 180; range(25:60,2) = 100;
+    range(41:49,1) = 180; range(41:49,2) = 90;
+    range(50:60,1) = 160; range(50:60,2) = 60;
+else
+    range( 1:30,1) = 170; range( 1:30,2) = 50;
+    range(31:60,1) = 140; range(31:60,2) = 50;
+end
+  
 plot_scales = true ;     % plot length scales
 col_spec    = "b-";      % colour for energy spectrum
-col_power   = "r-";      % colour for power law
+col_power   = "k-";      % colour for power law
 
 run_id = test_case+"J"+num2str(level,'%1.1d')+"Z"+num2str(zlevels,'%2.2d');  % naming convention
-tmpdir = "temp_spec"; mkdir(tmpdir);
+tmpdir = dir+"temp_spec"; mkdir(tmpdir)
+
+% Ensure each plot uses different colors
+figure;
+pbaspect([4 3 1]); 
+set(gca,'XScale','log','YScale','log');
+ax = gca;
+ax.ColorOrder = parula(numel(layers));    
+ax.ColorOrderIndex = 1;  
+hp = zeros(size(layers));
 
 pow_law = zeros(cp_max-cp_min+1,zlevels);
 ymin = 1e16; ymax = -1e16;
 for cp_id = cp_min:cp_max
     if ~avg
         cp = compose("%04d",cp_id);
-        tgzfile = run_id+"_"+cp+"_spec.tgz";
+        tgzfile = dir+run_id+"_"+cp+"_spec.tgz";
         
         fprintf('\nPower law exponents for checkpoint %d\n', cp_id)
     else
-        tgzfile = run_id+"_spec.tgz"; 
+        tgzfile = dir+run_id+"_spec.tgz"; 
     end 
     untar(tgzfile, tmpdir); % uncompress spectrum tar file
 
     fprintf("Layer     p")
-    for zlev = layers
-        % Load spectrum data
-        name_type = "Layer "+zlev;
-        k         = compose("%04d",zlev);
+    p = 1;
+    for zlev = fliplr(layers)
+        % Load data
+        k = compose("%04d",zlev);
         if avg % average spectrum
             file_base = run_id+"_"+k+"_"+type;
         else
@@ -72,8 +102,8 @@ for cp_id = cp_min:cp_max
         scales = 2*pi*radius/1e3./sqrt(pspec(:,1).*(pspec(:,1)+1)); % equivalent length scale (Jeans relation)
 
         % Fit power law
-        fit_indices = find(scales > range(2) & scales < range(1));
-        [P,S] = polyfit(log10(scales(fit_indices)),log10(pspec(fit_indices,2)),1);
+        fit_indices = find(scales > range(zlev,2) & scales < range(zlev,1));
+        [P,S] = polyfit(log10(scales(fit_indices)), log10(pspec(fit_indices,2)), 1);
         st_err = sqrt(diag(inv(S.R)*inv(S.R'))*S.normr^2/S.df); % error in coefficients from covariance matrix of P        
         
         %fprintf("\n %3.0f    %.2f +/- %.2f", zlev, -P(1), st_err(1));
@@ -82,9 +112,12 @@ for cp_id = cp_min:cp_max
         pow_law (cp_id,zlev) = -P(1);
         
         if plot_spec
-            loglog(scales, pspec(:,2),col_spec,"linewidth",3,"DisplayName",name_type);hold on;grid on;
+            name_type = "z = "+compose('%5.0f',Z(zlev))+" [m], p = "+compose('%2.1f', P(1));
+
+            hp(p) = loglog(scales, pspec(:,2), "linewidth", 3, "DisplayName", name_type); hold on; grid on
+            p = p+1;
             if power % plot fit
-                powerlaw (scales, 1.5*pspec(:,2), range, -P(1), col_power)
+                powerlaw (scales, 1.2*pspec(:,2), [range(zlev,1) range(zlev,2)], -P(1))
             end
         end
         ymin = min (ymin, 10^(floor(log10(min(pspec(:,2))))));
@@ -97,25 +130,29 @@ fprintf("\n")
 
 xmin = 10^(floor(log10(min(scales))));
 xmax = 10^(ceil(log10(max(scales))));
-axis([xmin xmax ymin ymax])
+axis([xmin xmax ymin ymax]); 
 
 if plot_scales
     if strcmp(test_case,"drake")
-        plot_scale(deltaI*KM,"\delta_{I}");
+        %plot_scale(deltaRh*KM,"\delta_{Rh}");
         %plot_scale(lambda1*KM,"\lambda_1");
-        plot_scale(deltaSM*KM,"\delta_{SM}");
+        %plot_scale(deltaSM*KM,"\delta_{SM}");
         %plot_scale(deltaM*KM,"\delta_{M}");
     elseif strcmp(test_case,"jet")
-        plot_scale(deltaI*KM,"\delta_{I}");
+        plot_scale(deltaRh*KM,"\delta_{R}");
         plot_scale(lambda1*KM,"\lambda_1");
         plot_scale(deltaSM*KM,"\delta_{M}");
     end
 end
 
 set (gca,"fontsize",20);
-xlabel("\lambda (km)");ylabel("S(\lambda)");
-set (gca,"Xdir","reverse"); %legend;
-%title("Run: "+run_id)
+xlabel("\lambda (km)");ylabel("S(\lambda)"); legend(hp,'Location', 'best','FontName', 'Menlo');
+set (gca,"Xdir","reverse");
+if tanh_strat
+    title("tanh stratification")
+else
+    title("Constant/linear stratification")
+end
 
 %% Plot power law profile with depth
 if drake
@@ -197,8 +234,8 @@ v_title = strip(type,'right','u')+" \bfu";
 plot_lon_lat_data(data(1:4:end,1:4:end), lon(1:4:end), lat(1:4:end), c_scale, v_title, smooth, shift, lines);
 axis(ax);
 %%
-function powerlaw (scales, power, range, p, col)
-% Plots power law -p between scales s1 and s2 (s2 > s1) with color col
+function powerlaw (scales, power, range, p)
+% Plots power law -p between scales s1 and s2 (s2 > s1) 
 [~,k1] = min(abs(scales-range(1))); [~,k2] = min(abs(scales-range(2))); knorm = round((k1+k2)/2);
 
 if abs(p+5/3) < 1e-2
@@ -209,25 +246,33 @@ else
     str = "k^{" + compose("%1.0f",p) + "}";
 end
 
-loglog(scales(k1:k2),scales(k1:k2).^(-p) * power(knorm)/scales(knorm)^(-p),col,"linewidth",3,"DisplayName",str);
+loglog(scales(k1:k2),scales(k1:k2).^(-p) * power(knorm)/scales(knorm)^(-p),...
+    'k', "linewidth", 3, "DisplayName", str);
 end
 
 function plot_scale (scale,name)
 y = ylim;
 x = scale;
-h = loglog([x x], y, "k","linewidth",1.5);
+h = loglog([x x], y, "linewidth",1.5,'k');
 set(get(get(h,"Annotation"),"LegendInformation"),"IconDisplayStyle","off");
 text(0.92*scale, 2.5*y(1), name, "fontsize", 16)
 end
 
-function [H, lambda0,lambda1, deltaS, deltaSM, deltaI, deltaM, radius] = params(test_case)
+function [H, lambda0,lambda1, deltaS, deltaSM, deltaRh, deltaM, radius, Z] = params(test_case)
 % Physical parameters of simulation
 
-global KM
+global KM; global tanh_strat
 
 if strcmp(test_case,"drake")
-    H_linear    = 3800;     % depth of linear scaling range (constant/linear)
-    %H_linear    = 300;      % depth of linear scaling range (tanh)
+    if tanh_strat
+        H_linear = 3800;     % depth of linear scaling range (constant/linear)
+    else
+        H_linear = 300;      % depth of linear scaling range (tanh)
+    end
+
+    H_mixed     =  200;     % depth of mixed layer
+    H_mode      = 1000;     % 
+
     uwbc        =  1.0;     % velocity scale 
     
     Laplace     =  2;       % 1 = Laplacian, 2 = bi-Laplacian
@@ -239,7 +284,7 @@ if strcmp(test_case,"drake")
     ref_density =  1030;
     H           =  4e3;     % full depth
     
-    visc        =  C_visc * dx^(2*Laplace)/dt;
+    visc        =  8.87e8;
     scale_omega =  6;
     scale_earth =  6;
     omega       =  7.29211e-5/scale_omega;
@@ -249,13 +294,11 @@ if strcmp(test_case,"drake")
     beta        =  2*omega*cos(deg2rad(theta))/radius;
     r_b         =  4e-4; % bottom friction
 
-    % Average Brunt-Vaisala frequency over full depth
-    %N_bv        = sqrt(-g * drho/ref_density * H_linear) / H; 
-    % Average Brunt-Vaisala frequency over linear layer
-    N_bv        = sqrt (-g/ref_density * drho/H_linear); 
-
+    % Brunt-Vaisala frequency
+    N_bv        = sqrt (- g * drho/ref_density / H_linear);
+    
     c0          = sqrt(g*H);
-    c1          = N_bv * H_linear / pi;
+    c1          = N_bv * H_mode / pi;
     deltaM      = (visc/beta)^(1/(2*Laplace+1)); % Munk layer
 elseif strcmp(test_case,"jet")
     visc        =  1.63e7; % hyperviscosity
@@ -264,7 +307,7 @@ elseif strcmp(test_case,"jet")
     radius      =  1000e3;
     g           =  9.80616;
     drho        = -4;
-    ref_density =  1027.8;
+    ref_density =  1027.8; 
     H           =  4e3;
     theta       =  45; % latitude at which to calculate f0 and beta
     f0          =  2*omega*sin(deg2rad(theta));
@@ -284,15 +327,22 @@ if strcmp(test_case,"drake")
     lambda1    = c1/f0;             % internal radius of deformation
     deltaS     = r_b/beta;          % Stommel layer
     deltaSM    = uwbc/f0;           % submesoscale
-    deltaI     = sqrt(uwbc/beta);   % inertial layer
-    Rey        = uwbc*deltaSM^(2*Laplace-1)/visc; % Reynolds number
+    deltaRh    = sqrt(uwbc/beta); % Rhine layer
+    Rey        = uwbc * deltaSM^(2*Laplace-1)/visc; % Reynolds number
     Ro         = uwbc / (deltaM*f0); % Rossby number
     fprintf('\nlambda0 = %2.1f km lambda1 = %2.1f km\n\n',lambda0*KM,lambda1*KM)
     fprintf('deltaS = %2.1f km deltaI = %2.1f km deltaM = %2.1f km deltaSM = %2.1f km\n\n',...
-        deltaS*KM, deltaI*KM, deltaM*KM, deltaSM*KM)
+        deltaS*KM, deltaRh*KM, deltaM*KM, deltaSM*KM)
     fprintf('Rey = %2.2e Ro = %2.2e N_bv = %2.2e\n\n', Rey, Ro, N_bv)
 end
+% Layer depths
+Z = [-3800 -3430 -3100 -2800 -2540 -2300 -2090 -1900 -1730 -1580 -1440 -1320 -1210 -1100 -1020 ...
+      -946 -874 -809 -751 -698 -650 -606 -566 -530 -497 -466 -438 -412 -389 -366 -346 -326 -308  ...
+      -291 -275 -260 -245 -231 -218 -205 -193 -181 -170 -159 -148 -138 -127 -117 -107 -97.4 -87.8 -78.3 -68.8 ...
+      -59.5 -50.2 -41.0 -31.9 -22.7 -13.6 -4.54];
 end
+
+
 
 
 
