3747ab8356e8faa21a5d9b17b61ca25c744b113a
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2026-01-20 10:31:22 -0500

    Modify boundary correction width.

--- post/paraview/xyz2lonlat.py
+++ post/paraview/xyz2lonlat.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
                         if (np.abs(np.abs(coord[0])-180)<2*dtheta):  # modify to avoid gaps if necessary
                             coord[0] = np.sign(coord[0]) * 180
                             points.SetPoint(pid, coord)
-                        if (np.abs(np.abs(coord[1])-90)<dtheta): # modify to avoid gaps if necessary
+                        if (np.abs(np.abs(coord[1])-90)<2*dtheta): # modify to avoid gaps if necessary
                             coord[1] = np.sign(coord[1]) * 90
                             points.SetPoint(pid, coord)
                 points.Modified()
