96056187beed69d02a0cc6b1cdfd7a4eed49640d
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2026-05-22 15:14:55 -0400

    Make coordinate arithmetic overload arithmetic elemental.

--- src/coord_arithmetic.f90
+++ src/coord_arithmetic.f90
@@ -30,49 +30,50 @@ module coord_arithmetic_mod
 
   interface operator (/)
      module procedure coord_divide_cr
+     module procedure coord_divide_cc
   end interface operator (/)
 contains
-  pure type(coord) function coord_plus_cc (c1, c2)
+  elemental type(coord) function coord_plus_cc (c1, c2)
     type(coord), intent (in) :: c1, c2
 
     coord_plus_cc = coord (c1%x+c2%x, c1%y+c2%y, c1%z+c2%z)
   end function coord_plus_cc
 
-  pure type(coord) function coord_plus_rc (c1, c2)
+  elemental type(coord) function coord_plus_rc (c1, c2)
     real(dp),    intent (in) :: c1
     type(coord), intent (in) :: c2
 
     coord_plus_rc = coord (c1+c2%x, c1+c2%y, c1+c2%z)
   end function coord_plus_rc
 
-  pure type(coord) function coord_plus_cr (c1, c2)
+  elemental type(coord) function coord_plus_cr (c1, c2)
     real(dp),    intent (in) :: c2
     type(coord), intent (in) :: c1
 
     coord_plus_cr = coord (c2+c1%x, c2+c1%y, c2+c1%z)
   end function coord_plus_cr
 
-  pure type(coord) function coord_minus_cc (c1, c2)
+  elemental type(coord) function coord_minus_cc (c1, c2)
     type(coord), intent (in) :: c1, c2
 
     coord_minus_cc = coord (c1%x-c2%x, c1%y-c2%y, c1%z-c2%z)
   end function coord_minus_cc
 
-  pure type(coord) function coord_minus_rc (c1, c2)
+  elemental type(coord) function coord_minus_rc (c1, c2)
     real(dp),    intent (in) :: c1
     type(coord), intent (in) :: c2
 
     coord_minus_rc = coord (c1-c2%x, c1-c2%y, c1-c2%z)
   end function coord_minus_rc
 
-  pure type(coord) function coord_minus_cr (c1, c2)
+  elemental type(coord) function coord_minus_cr (c1, c2)
     real(dp),    intent (in) :: c2
     type(coord), intent (in) :: c1
 
     coord_minus_cr = coord (c1%x-c2, c1%y-c2, c1%z-c2)
   end function coord_minus_cr
 
-  pure real(dp) function coord_times_cc (c1, c2)
+  elemental real(dp) function coord_times_cc (c1, c2)
     ! Inner product
     implicit none
     type(coord), intent (in) :: c1, c2
@@ -80,7 +81,7 @@ contains
     coord_times_cc = c1%x*c2%x + c1%y*c2%y + c1%z*c2%z
   end function coord_times_cc
 
-  pure type(coord) function coord_times_rc (c1, c2)
+  elemental type(coord) function coord_times_rc (c1, c2)
     implicit none
     real(dp),    intent (in) :: c1
     type(coord), intent (in) :: c2
@@ -88,7 +89,7 @@ contains
     coord_times_rc = coord (c1*c2%x, c1*c2%y, c1*c2%z)
   end function coord_times_rc
 
-  pure type(coord) function coord_times_cr (c1, c2)
+  elemental type(coord) function coord_times_cr (c1, c2)
     implicit none
     real(dp),    intent (in) :: c2
     type(coord), intent (in) :: c1
@@ -96,11 +97,18 @@ contains
     coord_times_cr = coord (c2*c1%x, c2*c1%y, c2*c1%z)
   end function coord_times_cr
 
-  pure type(coord) function coord_divide_cr (c1, c2)
+  elemental type(coord) function coord_divide_cr (c1, c2)
     implicit none
     real(dp),    intent (in) :: c2
     type(coord), intent (in) :: c1
 
     coord_divide_cr = coord (c1%x/c2, c1%y/c2, c1%z/c2)
   end function coord_divide_cr
+
+  elemental type(coord) function coord_divide_cc (c1, c2)
+    implicit none
+    type(coord), intent (in) :: c1, c2
+
+    coord_divide_cc = coord (c1%x/c2%x, c1%y/c2%y, c1%z/c2%z)
+  end function coord_divide_cc
 end module coord_arithmetic_mod
