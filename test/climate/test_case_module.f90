0dd661588246cc3a3aa06403e40df04fa5dd458f
Nicholas Kevlahan <kevlahan@mcmaster.ca>
2026-06-02 14:33:08 -0400

    Print out physics_model flag for climate test case.

--- test/climate/test_case_module.f90
+++ test/climate/test_case_module.f90
@@ -519,11 +519,13 @@ contains
             &*************************************************************'
        write (6,'(a)')        "RUN PARAMETERS"
        write (6,'(a,a)')      "test_case               = ", trim (test_case)
-       write (6,'(a,a)')      "physics_type            = ", trim (physics_type)
+       write (6,'(a,l1)')     "topo_test               = ", topo_test
+       write (6,'(a,l1)')     "physics_model           = ", physics_model
+       if (physics_model) &
+            write (6,'(a,a)')      "physics_type            = ", trim (physics_type)
        write (6,'(a,a)')      "run_id                  = ", trim (run_id)
        write (6,'(a,l1)')     "compressible            = ", compressible
        write (6,'(a,l1)')     "split_mean_perturbation = ", split_mean_perturbation
-       write (6,'(a,l1)')     "topo_test               = ", topo_test
        write (6,'(a,i3)')     "min_level               = ", min_level
        write (6,'(a,i3)')     "max_level               = ", max_level
        write (6,'(a,i5)')     "number of domains       = ", N_GLO_DOMAIN
